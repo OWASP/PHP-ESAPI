c27bab28676a47d5929a6ec65b8d2c427e3c3c0a
Andrew van der Stock <vanderaj@owasp.org>
2008-04-03 02:10:28 +0000

    First pass

--- src/org.owasp.esapi.ESAPI.php
+++ src/org.owasp.esapi.ESAPI.php
@@ -15,212 +15,225 @@ require_once("interfaces/org.owasp.esapi.ISecurityConfiguration.php");
 require_once("interfaces/org.owasp.esapi.IValidator.php");
 
 /**
- * @author rdawes
+ * @author vanderaj
  *
  */
 class ESAPI {
-
     private static $accessController = null;
-    
     private static $authenticator = null;
-    
     private static $encoder = null;
-    
     private static $encryptor = null;
-    
     private static $executor = null;
-    
     private static $httpUtilities = null;
-    
     private static $intrusionDetector = null;
-    
-//    private static ILogger logger = null;
-    
+	private static $logger = null;
     private static $randomizer = null;
-    
     private static $securityConfiguration = null;
-    
     private static $validator = null;
 
     /**
-     * prevent instantiation of this class
+     * Constructor (not necessary, but you can if you want)
      */
-    private function ESAPI() {}
+    function __construct() {
+    	
+    }
     
     /**
      * @return the accessController
      */
-    public static function accessController() {
-        if (ESAPI::accessController == null)
-            ESAPI::accessController = new AccessController();
-        return ESAPI::accessController;
+    public function accessController() {
+        if ($this->accessController == null) {
+        	$this->accessController = new AccessController();
+        }
+            
+        return $this->accessController;
     }
 
     /**
      * @param accessController the accessController to set
      */
-    public static function setAccessController($accessController) {
-        ESAPI::accessController = accessController;
+    public function setAccessController($accessController) {
+        $this->accessController = $accessController;
     }
 
     /**
      * @return the authenticator
      */
     public static function authenticator() {
-        if (ESAPI::authenticator == null)
-            ESAPI::authenticator = new Authenticator();
-        return ESAPI::authenticator;
+        if ($this->authenticator == null) {
+        	$this->authenticator = new Authenticator();
+        }
+            
+        return $this->authenticator;
     }
 
     /**
      * @param authenticator the authenticator to set
      */
     public static function setAuthenticator($authenticator) {
-        ESAPI::authenticator = authenticator;
+        $this->authenticator = $authenticator;
     }
 
     /**
      * @return the encoder
      */
     public static function encoder() {
-        if (ESAPI::encoder == null)
-            ESAPI::encoder = new Encoder();
-        return ESAPI::encoder;
+        if ($this->encoder == null) {
+        	$this->encoder = new Encoder();
+        }
+            
+        return $this->encoder;
     }
 
     /**
      * @param encoder the encoder to set
      */
     public static function setEncoder($encoder) {
-        ESAPI::encoder = encoder;
+        $this->encoder = $encoder;
     }
 
     /**
      * @return the encryptor
      */
     public static function encryptor() {
-        if (ESAPI::encryptor == null)
-            ESAPI::encryptor = new Encryptor();
-        return ESAPI::encryptor;
+        if ($this->encryptor == null) {
+        	$this->encryptor = new Encryptor();
+        }
+            
+        return $this->encryptor;
     }
 
     /**
      * @param encryptor the encryptor to set
      */
     public static function setEncryptor($encryptor) {
-        ESAPI::encryptor = encryptor;
+        $this->encryptor = $encryptor;
     }
 
     /**
      * @return the executor
      */
     public static function executor() {
-        if (ESAPI::executor == null)
-            ESAPI::executor = new Executor();
-        return ESAPI::executor;
+        if ($this->executor == null) {
+        	$this->executor = new Executor();
+        }
+            
+        return $this->executor;
     }
 
     /**
      * @param executor the executor to set
      */
     public static function setExecutor($executor) {
-        ESAPI::executor = executor;
+        $this->executor = $executor;
     }
 
     /**
      * @return the httpUtilities
      */
     public static function httpUtilities() {
-        if (ESAPI::httpUtilities == null)
-            ESAPI::httpUtilities = new HTTPUtilities();
-        return ESAPI::httpUtilities;
+        if ($this->httpUtilities == null) {
+        	$this->httpUtilities = new HTTPUtilities();
+        }
+            
+        return $this->httpUtilities;
     }
 
     /**
      * @param httpUtilities the httpUtilities to set
      */
     public static function setHttpUtilities($httpUtilities) {
-        ESAPI::httpUtilities = httpUtilities;
+        $this->httpUtilities = $httpUtilities;
     }
 
     /**
      * @return the intrusionDetector
      */
     public static function intrusionDetector() {
-        if (ESAPI::intrusionDetector == null)
-            ESAPI::intrusionDetector = new IntrusionDetector();
-        return ESAPI::intrusionDetector;
+        if ($this->intrusionDetector == null) {
+        	$this->intrusionDetector = new IntrusionDetector();
+        }
+            
+        return $this->intrusionDetector;
     }
 
     /**
      * @param intrusionDetector the intrusionDetector to set
      */
     public static function setIntrusionDetector($intrusionDetector) {
-        ESAPI::intrusionDetector = intrusionDetector;
+        $this->intrusionDetector = $intrusionDetector;
     }
 
-//    /**
-//     * @return the logger
-//     */
-//    public static  ILogger getLogger() {
-//        if (ESAPI::logger == null)
-//            return Logger();
-//        return ESAPI::logger;
-//    }
-//
-//    /**
-//     * @param logger the logger to set
-//     */
-//    public static  void setLogger(ILogger logger) {
-//        ESAPI::logger = logger;
-//    }
-//
+	/**
+	 * @return the logger
+     */
+	public static function getLogger() {
+		if ($this->logger == null) {
+			$this->logger = new Logger();
+		}
+			
+		return $this->logger;
+	}
+
+    /**
+     * @param logger the logger to set
+     */
+    public static function setLogger($logger) {
+        $this->logger = $logger;
+    }
+
     /**
      * @return the randomizer
      */
     public static function randomizer() {
-        if (ESAPI::randomizer == null)
-            ESAPI::randomizer = new Randomizer();
-        return ESAPI::randomizer;
+        if ($this->randomizer == null) {
+        	$this->randomizer = new Randomizer();
+        }
+            
+        return $this->randomizer;
     }
 
     /**
      * @param randomizer the randomizer to set
      */
     public static function setRandomizer($randomizer) {
-        ESAPI::randomizer = randomizer;
+        $this->randomizer = $randomizer;
     }
 
     /**
      * @return the securityConfiguration
      */
     public static function securityConfiguration() {
-        if (ESAPI::securityConfiguration == null)
-            ESAPI::securityConfiguration = new SecurityConfiguration();
-        return ESAPI::securityConfiguration;
+        if ($this->securityConfiguration == null) {
+        	$this->securityConfiguration = new SecurityConfiguration();
+        }
+            
+        return $this->securityConfiguration;
     }
 
     /**
      * @param securityConfiguration the securityConfiguration to set
      */
     public static function setSecurityConfiguration($securityConfiguration) {
-        ESAPI::securityConfiguration = securityConfiguration;
+        $this->securityConfiguration = $securityConfiguration;
     }
 
     /**
      * @return the validator
      */
     public static function validator() {
-        if (ESAPI::validator == null)
-            ESAPI::validator = new Validator();
-        return ESAPI::validator;
+        if ($this->validator == null) {
+        	$this->validator = new Validator();
+        }
+            
+        return $this->validator;
     }
 
     /**
      * @param validator the validator to set
      */
     public static function setValidator($validator) {
-        ESAPI::validator = validator;
+        $this->validator = $validator;
     }
     
 }
