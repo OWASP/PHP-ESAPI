83ccb561afc96a6059fe77d60f9f796a4d27da8e
Andrew van der Stock <vanderaj@owasp.org>
2008-08-30 17:35:46 +0000

    Updating to latest SimpleTest

--- lib/simpletest/eclipseupdate/trunk/samples/grouptest.php
+++
@@ -1,9 +0,0 @@
-<?php
-class myGroupTest extends GroupTest {
-    function myGroupTest() {
-		parent::GroupTest('');
-        $this->addTestFile(dirname(__FILE__).'/test1.php');
-		$this->addTestFile(dirname(__FILE__).'/test2.php');
-    }
-}
-?>
