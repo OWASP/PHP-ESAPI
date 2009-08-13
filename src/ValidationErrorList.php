1fe030969977e3b07b747fac7a7154d112ef19f1
Andrew van der Stock <vanderaj@owasp.org>
2009-08-13 08:51:11 +0000

    Update (C) notices

--- src/ValidationErrorList.php
+++ src/ValidationErrorList.php
@@ -6,7 +6,7 @@
  * Enterprise Security API (ESAPI) project. For details, please see
  * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - 2008 The OWASP Foundation
+ * Copyright (c) 2007 - 2009 The OWASP Foundation
  * 
  * The ESAPI is published by OWASP under the BSD license. You should read and accept the
  * LICENSE before you use, modify, and/or redistribute this software.
