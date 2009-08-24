1094e3de15177e4b79e1de89694b05fe39467270
jullrich@sans.edu <jullrich@sans.edu@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2009-08-24 17:08:16 +0000

    started implementation

---
+++ trunk/src/ValidationRule.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ * 
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * 
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ * 
+ * @author 
+ * @created 2008
+ * @since 1.4
+ * @package org.owasp.esapi
+ */
+
+require_once dirname(__FILE__).'/errors/IntrusionException.php';
+require_once dirname(__FILE__).'/errors/ValidationException.php';
+
+
+interface ValidationRule {
+	public function getAllowNull();
+	public function setTypeName($typeName);
+	public function setEncoder($encoder);
+	public function assertValid($context,$input);
+	public function getValid($context,$input, $errorList=null);
+	public function getSafe($context,$input);
+	public function isValid($context,$input);
+	public function whitelist($input,$list=null);
+}
+?>
