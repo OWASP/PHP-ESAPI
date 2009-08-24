8029b59ffbd60856d28cdfa10ecbebc9a74fb118
jullrich@sans.edu <jullrich@sans.edu@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2009-08-24 17:13:28 +0000

    started implementation. created these files to stay similar to java implementation

---
+++ trunk/src/reference/validation/BaseValidationRule.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ * 
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * 
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ * 
+ * @author Johannes B. Ullrich
+ * @created 2008
+ * @since 1.4
+ * @package org.owasp.esapi.reference
+ */
+
+require_once dirname ( __FILE__ ) . '/../../ValidationRule.php';
+
+abstract class BaseValidationRule implements ValidationRule {
+	private $typeName = null;
+	protected $allowNull = false;
+	protected $encoder = null;
+	
+	protected function BaseValidationRule($typeName, $encoder) {
+		if ($encoder) {
+			$this->encoder = $encoder;
+		} else {
+		    // TODO: need to figure out how to do this for PHP
+			// $this->encoder = ESAPI::encoder;
+		}
+		$this->typeName = $typeName;
+	}
+	
+	public function setAllowNull($flag) {
+		if ($flag) {
+			$this->allowNull = true;
+		} else {
+			$this->allowNull = false;
+		}
+	}
+	
+	public function getAllowNull() {
+		return $this->allowNull;
+	}
+	
+	public function getTypeName() {
+		return $this->typeName;
+	}
+	
+	public function setTypeName($typeName) {
+		$this->typeName ( $typeName );
+	}
+	
+	public function setEncoder($encoder) {
+		$this->encoder = $encoder;
+	}
+	
+	public function assertValid($context, $input) {
+		getValid ( $context, $input );
+	}
+	
+	public function getValid($context, $input, $errorList = null) {
+		try {
+			return getValid ( $context, $input );
+		} catch ( ValidationException $e ) {
+			$errorList->addError ( $context, $e );
+		}
+		return null;
+	}
+	public function getSafe($context, $input) {
+		try {
+			return $this->getValid ( $context, $input );
+		
+		} catch ( ValidationException $e ) {
+			return $this->sanitize ( $context, $input );
+		}
+	}
+	
+	public function isValid($context, $input) {
+		try {
+			getValid ( $context, $input );
+			return true;
+		} catch ( ValidationException $e ) {
+			return false;
+		} catch ( Exception $e ) {
+			return false;
+		}
+	}
+	// TODO: the "list" is not used in the java version. I assume it is suppose to provide a 
+	//       list of valid characters. Need to read up on specs to see how this is supposed to 
+	//       work. Right now, the java version appears to allow only digits which is what this
+	//	     function attempts to provide.
+	
+
+	public function whitelist($input, $list = null) {
+		return preg_replace ( "/[^0-9]/", '', $input );
+	}
+
+}
+?>
---
+++ trunk/src/reference/validation/CreditCardValidationRule.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ * 
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * 
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ * 
+ * @author Johannes B. Ullrich
+ * @created 2008
+ * @since 1.4
+ * @package org.owasp.esapi.reference
+ */
+
+require_once "BaseValidationRule.php";
+
+class CreditCardValidationRule extends BaseValidationRule {
+	private $ccrule=null;
+	public function CreditCardValidationRule ( $typeName, $encoder, $format=null) {
+		parent::BaseValidationRule($typeName, $encoder);
+		$this->ccrule=$this->getCCRule($encoder);
+	}
+	private function getCCRule($encoder) {
+		global $ESAPI;
+		$config = ESAPI::getSecurityConfiguration();
+		$pattern=$config->getValidationPattern("CreditCard");
+		$ccr = new StringValidationRule ( "ccrule", $this->encoder,$pattern );
+		$ccr->setMaximumLength(19);
+		$ccr->setAllowNull(false);
+		return $ccr;
+	}
+	
+	public function getValid($context,$input,$errolist=null ) {
+		if ( strlen($input)==0 ) {
+			if ( allowNull ) return null;
+			throw new ValidationException($context.": Input credit card required","Input credit card required: context=".$context.", input="+$input,$context);
+		}
+		$canonical=$this->ccrule->getValid($context,$input);
+		$canonical=preg_replace('/[^0-9]/','',$canonical);
+		$odd=!strlen($canonical)%2;
+		$sum=0;
+		for($i=0;$i<strlen($canonical);++$i) {
+			$n=0+$canonical[i];
+			$odd=!$odd;
+			if ( $odd) {
+				$sum+=$n;
+			} else {
+				$x=$n*2;
+				$sum+=$x>9?$x-9:$x;
+			}			
+		}
+		if ( ($sum%10)!=0 ) {
+			throw new ValidationException($context.": Invalid credit card input","Invalid credit card input: context="+context,context);
+		}
+		return $canonical;
+	}
+	public function sanitize ( $context, $input) {
+		// TODO: this code is broken. just a reminder to get it working.
+		global $ESAPI;
+		return $this->whitelist($input, $ESAPI->DefaultEncoder->CHAR_DIGITS);
+	}
+}
+?>
