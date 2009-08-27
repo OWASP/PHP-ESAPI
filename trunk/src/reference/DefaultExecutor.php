a3a806915a492585e23304efc5017b6378316cff
mike.boberski <mike.boberski@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2009-08-27 15:48:59 +0000

    A first implementation of DefaultExecutor

--- trunk/src/reference/DefaultExecutor.php
+++ trunk/src/reference/DefaultExecutor.php
@@ -20,7 +20,41 @@
 require_once  dirname(__FILE__).'/../Executor.php';
 
 class DefaultExecutor implements Executor {
+		
+	// Logger
 	
+	private $ApplicationName = null;
+	private $LogEncodingRequired = null;
+	private $LogLevel = null;
+	private $LogFileName = null;
+	private $MaxLogFileSize = null;
+	
+	// Codecs
+	
+	private $codec = null;
+	
+	function __construct()
+	{
+        //TODO: Use codecs
+		try {
+	       	if(strstr(php_uname('s'),'Windows')) {
+	       		$this->logSpecial( 'Using WindowsCodec for Executor. If this is not running on Windows this could allow injection');
+//	       	    $this->codec = new WindowsCodec();
+	       	}
+	       	else {
+	       		$this->logSpecial( 'Using UnixCodec for Executor. If this is not running on Unix this could allow injection');
+//	       	    $this->codec = new UnixCodec();
+	       	}       	
+        } 
+        catch ( Exception $e ) {
+        	$this->logSpecial($e->getMessage());
+        }
+	}
+	
+	private function logSpecial($msg) {
+		echo $msg;
+	}
+
     /**
      * Executes a system command after checking that the executable exists and
      * escaping all the parameters to ensure that injection is impossible.
@@ -31,20 +65,70 @@ class DefaultExecutor implements Executor {
      *            the command to execute
      * @param params
      *            the parameters of the command being executed
-     * @param workdir
-     *            the working directory
-     * @param codec
-     *            the codec to use to encode for the particular OS in use
-     * 
-     * @return the output of the command being run
      * 
      * @throws ExecutorException
      *             the service exception
-     */
-    function executeSystemCommand($executable, $params, $workdir, $codec) {
-    	throw new EnterpriseSecurityException("Method Not implemented");
-    }
+     */    
+	function executeSystemCommand($executable, $params) {
+    	$config = ESAPI::getSecurityConfiguration();
+    	$workdir = $config->getWorkingDirectory();
+    	$logParams = false;
+
+    	try {
+    		
+	        // executable must exist
+	        $resolved = $executable;
+			if(substr(PHP_OS, 0, 3) == 'WIN') {
+	        	$exploded = explode("%", $executable);
+	        	$systemroot = getenv($exploded[1]);
+	        	$resolved = $systemroot . $exploded[2];
+			}
+            if (!file_exists($resolved)) {
+                throw new ExecutorException("Execution failure, No such executable: $executable");
+            }
+            
+            // executable must use canonical path
+            if (!strcmp($resolved, realpath($resolved))) {
+                throw new ExecutorException("Execution failure, Attempt to invoke an executable using a non-absolute path: $executable");
+            }            
+                     		
+            // exact, absolute, canonical path to executable must be listed in ESAPI configuration 
+            $approved = $config->getAllowedExecutables();
+            if(!in_array($executable, $approved)){
+                throw new ExecutorException("Execution failure, Attempt to invoke executable that is not listed as an approved executable in ESAPI configuration: " . $executable . " not listed in " + $approved);
+            }            
+
+            // escape any special characters in the parameters
+            for ($i = 0; $i < count($params); $i++){
+            	$params[$i] = escapeshellcmd($params[$i]);  
+            }
+           
+            // working directory must exist
+	        $resolved_workdir = $workdir;
+			if(substr(PHP_OS, 0, 3) == 'WIN') {
+	        	$exploded = explode("%", $workdir);
+	        	$systemroot = getenv($exploded[1]);
+	        	$resolved_workdir = $systemroot . $exploded[2];
+			}
+             if(!file_exists($resolved_workdir)){
+                throw new ExecutorException("Execution failure, No such working directory for running executable: $workdir");
+            }
+ 
+            // run the command
+            $paramstr = "";
+            foreach($params as $param) {
+            	$paramstr = $paramstr." ".$param;
+            }
+            $this->logSpecial( "Initiating executable: $executable $paramstr in $workdir");
+            $output = `$executable $paramstr`;
+            $this->logSpecial( "System command complete");
+            return $output;
+    	}
+    	catch ( Exception $e ) {
+        	$this->logSpecial($e->getMessage());
+        }
 	
+	}	
 	
 }
 ?>
