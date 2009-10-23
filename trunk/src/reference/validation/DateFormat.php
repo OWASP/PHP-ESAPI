e881a0064fcb5db75a5cddf969936ce866dc68dc
vanderaj <vanderaj@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2009-10-23 10:22:37 +0000

    Fix for defect http://code.google.com/p/owasp-esapi-php/issues/detail?id=19
    Changes 
    * Fixes typo
    * Fixes bug with const syntax (can't have private const's)
    * move to self::types (correct syntax for PHP 5 constants) rather than $this->types (incorrect)

--- trunk/src/reference/validation/DateFormat.php
+++ trunk/src/reference/validation/DateFormat.php
@@ -1,24 +1,26 @@
 <?php
 
 class DateFormat {
-	private $format=array();
-	const private $types=array('SMALL','MEDIUM','LONG','FULL');
-	function __construct($format=null,$type='MEDIUM') {
-		Da
+	private $format = array();
+	const types = array('SMALL','MEDIUM','LONG','FULL');
+	
+	function __construct($format=null, $type='MEDIUM') {
 		$this->setformat($format,$type);
 	}
-	function setformat($format,$type='MEDIUM') {
+	
+	function setformat($format, $type='MEDIUM') {
+		
 		if ( is_array($format)) {
-			foreach ( $this->types as $t ) {
-				if ( key_exists($t,$format)) {
-					$this->format[$t]=$format[$t];		
+			foreach ( self::types as $t ) {
+				if ( key_exists($t, $format)) {
+					$this->format[$t] = $format[$t];		
 				}
 			}
 		} else {
-			if ( in_array($type,$types) {
-			$this->format[$type]=$format;
+			if ( in_array($type, self::types) ) {
+				$this->format[$type] = $format;
 			} else {
-				throw ValidationException("invalid date type ".$type);
+				throw ValidationException("invalid date type " . $type);
 			}						
 		}
 	}
