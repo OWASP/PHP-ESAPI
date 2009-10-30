7066f876ba6a54c0b350444c760374f04c5a54d2
vanderaj <vanderaj@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2009-10-30 06:11:45 +0000

    Updated author in (C) notes. Please replace me or add yourself if you make significant changes.

--- trunk/test/codecs/CSSCodecTest.php
+++ trunk/test/codecs/CSSCodecTest.php
@@ -6,13 +6,13 @@
  * Enterprise Security API (ESAPI) project. For details, please see
  * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - The OWASP Foundation
+ * Copyright (c) 2009 The OWASP Foundation
  *
  * The ESAPI is published by OWASP under the BSD license. You should read and accept the
  * LICENSE before you use, modify, and/or redistribute this software.
  *
- * @author Jeff Williams <a href="http://www.aspectsecurity.com">Aspect Security</a>
- * @created 2007
+ * @author Andrew van der Stock < van der aj ( at ) owasp. org >
+ * @created 2009
  */
 
 require_once dirname(__FILE__).'/../../src/ESAPI.php';
