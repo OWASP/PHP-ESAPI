e2755c3dde13809578ba3dad7dfe2a0ca10565c5
bipin.code <bipin.code@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2009-11-24 18:55:43 +0000

    --> Couple of Authenticator tests before going to bed :)

--- trunk/test/reference/AuthenticatorTest.php
+++ trunk/test/reference/AuthenticatorTest.php
@@ -1,34 +1,100 @@
 <?php
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
+ *
  * This file is part of the Open Web Application Security Project (OWASP)
  * Enterprise Security API (ESAPI) project. For details, please see
  * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
  * Copyright (c) 2007 - 2009 The OWASP Foundation
- * 
+ *
  * The ESAPI is published by OWASP under the BSD license. You should read and accept the
  * LICENSE before you use, modify, and/or redistribute this software.
- * 
+ *
  * @author Andrew van der Stock (vanderaj @ owasp.org)
  * @created 2009
  */
-class AuthenticatorTest extends UnitTestCase 
-{
-	function setUp() 
-	{
-		
-	}
-	
-	function tearDown()
-	{
-		
-	}
-	
-	function testFail()
-	{
-		$this->fail();
-	}
+class AuthenticatorTest extends UnitTestCase {
+
+        private $CHAR_ALPHANUMERICS = 'abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ01234567890';
+
+        function setUp() {
+                global $ESAPI;
+
+                if ( !isset($ESAPI)) {
+                        $ESAPI = new ESAPI(dirname(__FILE__).'/../testresources/ESAPI.xml');
+                }
+        }
+
+        function tearDown() {
+
+        }
+
+        function testCreatUserWithAnExistingAccountName() {
+                $accountName = ESAPI::getRandomizer()->getRandomString(8, $this->CHAR_ALPHANUMERICS);
+                $authenticator = ESAPI::getAuthenticator();
+                $password = $authenticator->generateStrongPassword();
+                $user = $authenticator->createUser($accountName, $password, $password);
+                $this->assertTrue($user->verifyPassword($password));
+
+                try {
+                        $authenticator->createUser($accountName, $password, $password);
+                        $this->fail();
+                }catch (AuthenticationException $e ) {
+                        $this->pass();
+                }
+        }
+
+        public function testCreatUserWithNonMatchingPasswords() {
+                $authenticator = ESAPI::getAuthenticator();
+                try {
+                        $authenticator->createUser(ESAPI::getRandomizer()->getRandomString(8, $this->CHAR_ALPHANUMERICS), "password1", "password2");
+                        $this->fail();
+                } catch (AuthenticationException $e) {
+                        $this->pass();
+                }
+        }
+
+        public function testCreatUserWithWeakPassword() {
+                $authenticator = ESAPI::getAuthenticator();
+                try {
+                        $authenticator->createUser(ESAPI::getRandomizer()->getRandomString(8, $this->CHAR_ALPHANUMERICS), "weak", "weak");
+                        $this->fail();
+                } catch (AuthenticationException $e) {
+                        $this->pass();
+                }
+        }
+
+        public function testCreatUserWithNullUserName() {
+                $authenticator = ESAPI::getAuthenticator();
+                try {
+                        $authenticator->createUser(null, "sdb5572g^&^", "sdb5572g^&^");
+                        $this->fail();
+                } catch (AuthenticationException $e) {
+                        $this->pass();
+                }
+        }
+
+        public function testCreatUserWithNullPassword() {
+                $authenticator = ESAPI::getAuthenticator();
+                try {
+                        $authenticator->createUser(ESAPI::getRandomizer()->getRandomString(8, $this->CHAR_ALPHANUMERICS), null, null);
+                        $this->fail();
+                } catch (AuthenticationException $e) {
+                        $this->pass();
+                }
+        }
+
+        public function testCreatUserWithStrongPassword() {
+                $authenticator = ESAPI::getAuthenticator();
+                $accountName = ESAPI::getRandomizer()->getRandomString(8, $this->CHAR_ALPHANUMERICS);
+                $password = ESAPI::getRandomizer()->getRandomString(15, $this->CHAR_ALPHANUMERICS);
+                try {
+                        $authenticator->createUser($accountName, $password, $password);
+                        $this->pass();
+                } catch (AuthenticationException $e) {
+                        $this->fail();
+                }
+        }
 }
 ?>
