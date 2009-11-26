12690e45611190e974e0f9d4515bf5f4c3d7e189
jahboite <jahboite@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2009-11-26 15:40:53 +0000

    Initial version of DefaultIntrusionDetector plus the addition of an Event class to the IntrusionDetector interface file and two tests in IntrusionDetectorTest.

--- trunk/src/reference/DefaultIntrusionDetector.php
+++ trunk/src/reference/DefaultIntrusionDetector.php
@@ -11,8 +11,8 @@
  * The ESAPI is published by OWASP under the BSD license. You should read and accept the
  * LICENSE before you use, modify, and/or redistribute this software.
  * 
- * @author 
- * @created 2008
+ * @author jah (at jahboite.co.uk)
+ * @created 2009
  * @since 1.4
  * @package org.owasp.esapi.reference
  */
@@ -21,6 +21,15 @@ require_once dirname(__FILE__).'/../IntrusionDetector.php';
 
 class DefaultIntrusionDetector implements IntrusionDetector {
 	
+    private $logger     = null;
+    private $userEvents = null;
+    
+    function __construct()
+    {
+    	$this->logger = ESAPI::getLogger('IntrusionDetector');
+    	$this->userEvents = array();
+    }
+    
     /**
      * Adds the exception to the IntrusionDetector.  This method should immediately log the exception so that developers throwing an 
      * IntrusionException do not have to remember to log every error.  The implementation should store the exception somewhere for the current user
@@ -33,7 +42,48 @@ class DefaultIntrusionDetector implements IntrusionDetector {
      * @throws IntrusionException 
      * 		the intrusion exception
      */
-    function addException($exception)	{ 			 	}
+    public function addException($exception)
+    {
+    	if (is_a($exception, 'EnterpriseSecurityException'))
+    	{
+    		$this->logger->warning( DefaultLogger::SECURITY, false, $exception->getLogMessage(), $exception );
+    	}
+    	else
+    	{
+    		$this->logger->warning( DefaultLogger::SECURITY, false, $exception->getMessage(), $exception );
+    	}
+    	
+    	 // FIXME: when getCurrentUser() is implemented, remove next 1 line which prevents infinite loop of addException() calls!
+    	while (1) return;
+    	    	
+    	// add the exception to the current user, which may trigger a detector 
+		$user = ESAPI::getAuthenticator()->getCurrentUser();
+    	$eventName = 'org.owasp.esapi.errors.' . get_class($exception);
+    	
+    	if (is_a($exception, 'IntrusionException'))
+        {
+        	return;
+        }
+        
+        // add the exception to the user's store, handle IntrusionException if thrown
+        try
+        {
+			$this->addSecurityEvent($user, $eventName);
+		}
+		catch (IntrusionException $intrusionException)
+		{
+			$quota = ESAPI::getSecurityConfiguration()->getQuota($eventName);
+            $actionsList = '[' . implode(', ', $quota->actions) . ']';
+            foreach ($quota->actions as $action)
+            {
+            	$message = 'User exceeded quota of ' . $quota->count . ' per ' . $quota->interval .
+            	 			' seconds for event ' . $eventName . '. Taking actions ' . $actionsList;
+            	$this->takeSecurityAction($action, $message);
+            }
+		}
+        
+        
+    }
 
     /**
      * Adds the event to the IntrusionDetector.  This method should immediately log the event.  The implementation should store the event somewhere for the current user
@@ -48,8 +98,97 @@ class DefaultIntrusionDetector implements IntrusionDetector {
      * @throws IntrusionException 
      * 		the intrusion exception
      */
-    function addEvent($eventName, $logMessage)	{ 			 	}
+    public function addEvent($eventName, $logMessage)
+    {
+    	$this->logger->warning( DefaultLogger::SECURITY, false, 'Security event ' . $eventName . ' received : ' . $logMessage);
+    	
+    	// add the event to the current user, which may trigger a detector
+    	$user = ESAPI::getAuthenticator()->getCurrentUser();
+    	try
+    	{
+    		$this->addSecurityEvent($user, 'event.' . $eventName);
+    	}
+    	catch (IntrusionException $intrusionException)
+    	{
+    		$quota = ESAPI::getSecurityConfiguration()->getQuota('event.' . $eventName);
+    		$actionsList = '[' . implode(', ', $quota->actions) . ']';
+    		foreach ($quota->actions as $action)
+    		{
+    			$message = 'User exceeded quota of ' . $quota->count . ' per ' . $quota->interval .
+            	 			' seconds for event ' . $eventName . '. Taking actions ' . $actionsList;
+    			$this->takeSecurityAction($action, $message);
+    		}
+    	}
+    }
 	
-	
+	/**
+     * Take a specified security action.  In this implementation, acceptable
+     * actions are: log, disable, logout.
+     * 
+     * @param action
+     * 		the action to take (log, disable, logout)
+     * @param message
+     * 		the message to log if the action is "log"
+     */
+    private function takeSecurityAction($action, $message)
+	{
+		if ($action === 'log' )
+		{
+			$this->logger->fatal(DefaultLogger::SECURITY, false, 'INTRUSION - ' . $message);
+		}
+		$user = ESAPI::getAuthenticator()->getCurrentUser();
+    	    	
+		if ($user === DefaultUser::ANONYMOUS )
+		{
+			return;
+		}
+		if ($action === 'disable' )
+		{
+			$user->disable();
+		}
+		if ($action === 'logout' )
+		{
+			$user->logout();
+		}
+	}
+    
+ 	 /**
+	 * Adds a security event to the user.  These events are used to check that the user has not
+	 * reached the security thresholds set in the properties file.
+	 * 
+	 * @param user
+	 * 			The user that caused the event.
+	 * @param eventName
+	 * 			The name of the event that occurred.
+	 */
+	private function addSecurityEvent($user, $eventName)
+	{
+		$events = null;
+		if (array_key_exists($user->getAccountName(), $this->userEvents))
+		{
+			$events = $this->userEvents[$user->getAccountName()];
+		}
+		if (get_type($events) != 'array' )
+		{
+			$events = array();
+			$this->userEvents[$user->getAccountName()] = $events;
+		}
+		$event = null;
+		if (array_key_exists($events, $eventName))
+		{
+			$event = $events[$eventName];
+		}
+		if (!is_a($event, 'Event'))
+		{
+			$event = new Event($eventName);
+			$events[$eventName] = $event;
+		}
+		
+		$q = ESAPI::getSecurityConfiguration()->getQuota($eventName);
+		if ($q->count > 0)
+		{
+			$event->increment($q->count, $q->interval);
+		}
+	}	
 }
 ?>
--- trunk/test/reference/IntrusionDetectorTest.php
+++ trunk/test/reference/IntrusionDetectorTest.php
@@ -11,24 +11,79 @@
  * The ESAPI is published by OWASP under the BSD license. You should read and accept the
  * LICENSE before you use, modify, and/or redistribute this software.
  * 
- * @author Andrew van der Stock (vanderaj @ owasp.org)
+ * @author jah (at jahboite.co.uk)
  * @created 2009
  */
+
+require_once dirname(__FILE__) . '/../http/TestHttpServletRequest.php';
+require_once dirname(__FILE__) . '/../http/TestHttpServletResponse.php';
+
 class IntrusionDetectorTest extends UnitTestCase 
 {
+	// FIXME: remove the next 1 line when DefaultEncoder::CHAR_ALPHANUMERICS is avalable
+	private $CHAR_ALPHANUMERICS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890';	
+	
 	function setUp() 
 	{
-		
+		global $ESAPI;
+		if (!isset($ESAPI)) 
+		{
+			$ESAPI = new ESAPI(dirname(__FILE__).'/../testresources/ESAPI.xml');
+		}
 	}
 	
-	function tearDown()
+	function tearDown() {}
+	
+	/**
+	 * Test of addException method, of class org.owasp.esapi.IntrusionDetector.
+	 * 
+	 * @throws AuthenticationException
+	 *             the authentication exception
+	 */
+	function testAddException()
 	{
-		
+		ESAPI::getIntrusionDetector()->addException(new IntrusionException("user message", "log message"));
+		$username = ESAPI::getRandomizer()->getRandomString(8, $this->CHAR_ALPHANUMERICS); // FIXME: DefaultEncoder::CHAR_ALPHANUMERICS
+		$auth = ESAPI::getAuthenticator();
+		$user = $auth->createUser($username, 'addException', 'addException');
+		$user->enable();
+		$request  = null; // FIXME: new TestHttpServletRequest();
+		$response = null; // FIXME: new TestHttpServletResponse();
+		ESAPI::getHttpUtilities()->setCurrentHTTP($request, $response);
+		$user->loginWithPassword('addException');
+			
+		// Now generate some exceptions to disable account
+		for ( $i = 0; $i < ESAPI::getSecurityConfiguration()->getQuota('org.owasp.esapi.errors.IntegrityException')->count; $i++ )
+		{
+			// EnterpriseSecurityExceptions are added to IntrusionDetector automatically
+			new IntegrityException("IntegrityException " + $i, "IntegrityException " + $i);
+		}
+		$this->assertFalse($user->isLoggedIn());
 	}
 	
-	function testFail()
-	{
-		$this->fail();
-	}
+	/**
+	 * Test of addEvent method, of class org.owasp.esapi.IntrusionDetector.
+	 *
+	 * @throws AuthenticationException
+	 *             the authentication exception
+	 */
+    function testAddEvent()
+    {
+        $username = ESAPI::getRandomizer()->getRandomString(8, $this->CHAR_ALPHANUMERICS); // FIXME: DefaultEncoder::CHAR_ALPHANUMERICS
+        $auth = ESAPI::getAuthenticator();
+		$user = $auth->createUser($username, 'addEvent', 'addEvent');
+		$user->enable();
+		$request  = null; // FIXME: new TestHttpServletRequest();
+		$response = null; // FIXME: new TestHttpServletResponse();
+		ESAPI::getHttpUtilities()->setCurrentHTTP($request, $response);
+		$user->loginWithPassword('addEvent');
+        
+        // Now generate some events to disable user account
+        for ( $i = 0; $i < ESAPI::getSecurityConfiguration()->getQuota('event.test')->count; $i++ )
+        {
+            ESAPI::getIntrusionDetector()->addEvent("test", "test message");
+        }
+        $this->assertFalse($user->isEnabled());
+    }
 }
 ?>
