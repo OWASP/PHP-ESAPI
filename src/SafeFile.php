3889a7f74d3efbac48d40e659147de12b5bde843
Martin Reiche <martin.reiche@uni-oldenburg.de>
2010-02-05 23:48:05 +0000

    Corrected indentation in SafeFile.php

--- src/SafeFile.php
+++ src/SafeFile.php
@@ -37,49 +37,49 @@ class SafeFile extends SplFileObject {
          * @param String $path the path to the file (path && file name)
          */
 
-	function __construct($path) {
-		parent::__construct($path);
-		$this->doDirCheck($this->getPath());
-		$this->doFileCheck($this->getFilename());
-	}
+        function __construct($path) {
+            parent::__construct($path);
+            $this->doDirCheck($this->getPath());
+            $this->doFileCheck($this->getFilename());
+        }
 
-	/**
+        /**
          * Checks the directory against null bytes and unprintable characters.
          * @param String $path the directory path (without the file name)
          */
-	private function doDirCheck($path) {
-		if ( preg_match($this->DIR_BLACKLIST_PAT, $path) ) {
-			throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains illegal character. ");
-		}
+        private function doDirCheck($path) {
+            if ( preg_match($this->DIR_BLACKLIST_PAT, $path) ) {
+                throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains illegal character. ");
+            }
 
-		if ( preg_match($this->PERCENTS_PAT, $path) ) {
-			throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains encoded characters. ");
-		}
-		
-		$ch = $this->containsUnprintableCharacters($path);
-		if (ch != -1) {
-			throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains unprintable character. ");
-		}
-	}
-	
+            if ( preg_match($this->PERCENTS_PAT, $path) ) {
+                throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains encoded characters. ");
+            }
+
+            $ch = $this->containsUnprintableCharacters($path);
+            if (ch != -1) {
+                throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains unprintable character. ");
+            }
+        }
+
         /**
          * Checks the file name against null bytes and unprintable characters.
          * @param String $path the file name
          */
-	private function doFileCheck($path) {
-		if ( preg_match($this->FILE_BLACKLIST_PAT, $path) ) {
-			throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains illegal character.");
-		}
+        private function doFileCheck($path) {
+            if ( preg_match($this->FILE_BLACKLIST_PAT, $path) ) {
+                throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains illegal character.");
+            }
 
-		if ( preg_match($this->PERCENTS_PAT, $path) ) {
-			throw new ValidationException("Invalid file", "File path (" + $path + ") contains encoded characters.");
-		}
-		
-		$ch = $this->containsUnprintableCharacters($path);
-		if (ch != -1) {
-			throw new ValidationException("Invalid file", "File path (" + $path + ") contains unprintable character.");
-		}
-	}
+            if ( preg_match($this->PERCENTS_PAT, $path) ) {
+                throw new ValidationException("Invalid file", "File path (" + $path + ") contains encoded characters.");
+            }
+
+            $ch = $this->containsUnprintableCharacters($path);
+            if (ch != -1) {
+                throw new ValidationException("Invalid file", "File path (" + $path + ") contains unprintable character.");
+            }
+        }
 
         /**
          * Checks the specified string for unprintable characters (ASCII range
@@ -88,13 +88,13 @@ class SafeFile extends SplFileObject {
          * @return int  the value of the first unprintable character found.
          *              If no unprintable character was found, -1 is returned.
          */
-	private function containsUnprintableCharacters($s) {
-		for ($i = 0; $i < strlen($s); $i++) {
-			$ch = $s[i];
-			if (ord($ch) < 32 || ord($ch) > 126) {
-				return $ch;
-			}
-		}
-		return -1;
-	}
+        private function containsUnprintableCharacters($s) {
+            for ($i = 0; $i < strlen($s); $i++) {
+                $ch = $s[i];
+                if (ord($ch) < 32 || ord($ch) > 126) {
+                    return $ch;
+                }
+            }
+            return -1;
+        }
 }
