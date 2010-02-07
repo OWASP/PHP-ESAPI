fb8b38e8a7cf628dc814a240f328ca9fdc14ea24
arnaud.labenne <arnaud.labenne@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-02-07 20:24:10 +0000

    Implemented DefaultEncryptor using mcrypt Modified Encryptor interface to fit the new requirements Added tests in EncryptorTest

--- trunk/src/Encryptor.php
+++ trunk/src/Encryptor.php
@@ -8,13 +8,12 @@
  *
  * Copyright (c) 2007 - 2009 The OWASP Foundation
  * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
- * 
- * @author 
+ * The ESAPI is published by OWASP under the BSD license. You should read and 
+ * accept the LICENSE before you use, modify, and/or redistribute this software.
+ *  
  * @created 2008
- * @since 1.4
- * @package org.owasp.esapi
+ * @since  1.4
+ * @author Arnaud Labenne - www.dotsafe.fr
  */
 
 require_once dirname(__FILE__).'/errors/EncryptionException.php';
@@ -35,158 +34,205 @@ require_once dirname(__FILE__).'/errors/IntegrityException.php';
  * <LI>encryptFile</LI>
  * </UL>
  * 
- * @author 
- * @since 1.4
+ * @author Arnaud Labenne - www.dotsafe.fr
+ * @since  1.4
  */
-interface Encryptor {
+interface Encryptor
+{
+    
+    //getCrypto supports 
+    const ESAPI_CRYPTO_OP_ENCRYPT_ASCII_HEX = 'ESAPI_CRYPTO_OP_ENCRYPT_ASCII_HEX';
+    const ESAPI_CRYPTO_OP_DECRYPT_ASCII_HEX = 'ESAPI_CRYPTO_OP_DECRYPT_ASCII_HEX';
+    const ESAPI_CRYPTO_OP_HASH_ASCII_HEX = 'ESAPI_CRYPTO_OP_HASH_ASCII_HEX';
+    
+    //A long list of supported encryption algorithm
+    const ESAPI_CRYPTO_MODE_SHA1 = 'ESAPI_CRYPTO_MODE_SHA1';
+    const ESAPI_CRYPTO_MODE_CAST_128_CBC = 'ESAPI_CRYPTO_MODE_CAST-128_CBC';
+    const ESAPI_CRYPTO_MODE_CAST_128_CFB = 'ESAPI_CRYPTO_MODE_CAST-128_CFB';
+    const ESAPI_CRYPTO_MODE_CAST_128_CTR = 'ESAPI_CRYPTO_MODE_CAST-128_CTR';
+    const ESAPI_CRYPTO_MODE_CAST_128_ECB = 'ESAPI_CRYPTO_MODE_CAST-128_ECB';
+    const ESAPI_CRYPTO_MODE_CAST_128_NCFB = 'ESAPI_CRYPTO_MODE_CAST-128_NCFB';
+    const ESAPI_CRYPTO_MODE_CAST_128_NOFB = 'ESAPI_CRYPTO_MODE_CAST-128_NOFB';
+    const ESAPI_CRYPTO_MODE_CAST_128_OFB = 'ESAPI_CRYPTO_MODE_CAST-128_OFB';
+    const ESAPI_CRYPTO_MODE_CAST_128_STREAM = 'ESAPI_CRYPTO_MODE_CAST-128_STREAM';
+    const ESAPI_CRYPTO_MODE_GOST_CBC = 'ESAPI_CRYPTO_MODE_GOST_CBC';
+    const ESAPI_CRYPTO_MODE_GOST_CFB = 'ESAPI_CRYPTO_MODE_GOST_CFB';
+    const ESAPI_CRYPTO_MODE_GOST_CTR = 'ESAPI_CRYPTO_MODE_GOST_CTR';
+    const ESAPI_CRYPTO_MODE_GOST_ECB = 'ESAPI_CRYPTO_MODE_GOST_ECB';
+    const ESAPI_CRYPTO_MODE_GOST_NCFB = 'ESAPI_CRYPTO_MODE_GOST_NCFB';
+    const ESAPI_CRYPTO_MODE_GOST_NOFB = 'ESAPI_CRYPTO_MODE_GOST_NOFB';
+    const ESAPI_CRYPTO_MODE_GOST_OFB = 'ESAPI_CRYPTO_MODE_GOST_OFB';
+    const ESAPI_CRYPTO_MODE_GOST_STREAM = 'ESAPI_CRYPTO_MODE_GOST_STREAM';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_128_CBC = 'ESAPI_CRYPTO_MODE_RIJNDAEL-128_CBC';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_128_CFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-128_CFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_128_CTR = 'ESAPI_CRYPTO_MODE_RIJNDAEL-128_CTR';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_128_ECB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-128_ECB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_128_NCFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-128_NCFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_128_NOFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-128_NOFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_128_OFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-128_OFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_128_STREAM = 'ESAPI_CRYPTO_MODE_RIJNDAEL-128_STREAM';
+    const ESAPI_CRYPTO_MODE_TWOFISH_CBC = 'ESAPI_CRYPTO_MODE_TWOFISH_CBC';
+    const ESAPI_CRYPTO_MODE_TWOFISH_CFB = 'ESAPI_CRYPTO_MODE_TWOFISH_CFB';
+    const ESAPI_CRYPTO_MODE_TWOFISH_CTR = 'ESAPI_CRYPTO_MODE_TWOFISH_CTR';
+    const ESAPI_CRYPTO_MODE_TWOFISH_ECB = 'ESAPI_CRYPTO_MODE_TWOFISH_ECB';
+    const ESAPI_CRYPTO_MODE_TWOFISH_NCFB = 'ESAPI_CRYPTO_MODE_TWOFISH_NCFB';
+    const ESAPI_CRYPTO_MODE_TWOFISH_NOFB = 'ESAPI_CRYPTO_MODE_TWOFISH_NOFB';
+    const ESAPI_CRYPTO_MODE_TWOFISH_OFB = 'ESAPI_CRYPTO_MODE_TWOFISH_OFB';
+    const ESAPI_CRYPTO_MODE_TWOFISH_STREAM = 'ESAPI_CRYPTO_MODE_TWOFISH_STREAM';
+    const ESAPI_CRYPTO_MODE_ARCFOUR_CBC = 'ESAPI_CRYPTO_MODE_ARCFOUR_CBC';
+    const ESAPI_CRYPTO_MODE_ARCFOUR_CFB = 'ESAPI_CRYPTO_MODE_ARCFOUR_CFB';
+    const ESAPI_CRYPTO_MODE_ARCFOUR_CTR = 'ESAPI_CRYPTO_MODE_ARCFOUR_CTR';
+    const ESAPI_CRYPTO_MODE_ARCFOUR_ECB = 'ESAPI_CRYPTO_MODE_ARCFOUR_ECB';
+    const ESAPI_CRYPTO_MODE_ARCFOUR_NCFB = 'ESAPI_CRYPTO_MODE_ARCFOUR_NCFB';
+    const ESAPI_CRYPTO_MODE_ARCFOUR_NOFB = 'ESAPI_CRYPTO_MODE_ARCFOUR_NOFB';
+    const ESAPI_CRYPTO_MODE_ARCFOUR_OFB = 'ESAPI_CRYPTO_MODE_ARCFOUR_OFB';
+    const ESAPI_CRYPTO_MODE_ARCFOUR_STREAM = 'ESAPI_CRYPTO_MODE_ARCFOUR_STREAM';
+    const ESAPI_CRYPTO_MODE_CAST_256_CBC = 'ESAPI_CRYPTO_MODE_CAST-256_CBC';
+    const ESAPI_CRYPTO_MODE_CAST_256_CFB = 'ESAPI_CRYPTO_MODE_CAST-256_CFB';
+    const ESAPI_CRYPTO_MODE_CAST_256_CTR = 'ESAPI_CRYPTO_MODE_CAST-256_CTR';
+    const ESAPI_CRYPTO_MODE_CAST_256_ECB = 'ESAPI_CRYPTO_MODE_CAST-256_ECB';
+    const ESAPI_CRYPTO_MODE_CAST_256_NCFB = 'ESAPI_CRYPTO_MODE_CAST-256_NCFB';
+    const ESAPI_CRYPTO_MODE_CAST_256_NOFB = 'ESAPI_CRYPTO_MODE_CAST-256_NOFB';
+    const ESAPI_CRYPTO_MODE_CAST_256_OFB = 'ESAPI_CRYPTO_MODE_CAST-256_OFB';
+    const ESAPI_CRYPTO_MODE_CAST_256_STREAM = 'ESAPI_CRYPTO_MODE_CAST-256_STREAM';
+    const ESAPI_CRYPTO_MODE_LOKI97_CBC = 'ESAPI_CRYPTO_MODE_LOKI97_CBC';
+    const ESAPI_CRYPTO_MODE_LOKI97_CFB = 'ESAPI_CRYPTO_MODE_LOKI97_CFB';
+    const ESAPI_CRYPTO_MODE_LOKI97_CTR = 'ESAPI_CRYPTO_MODE_LOKI97_CTR';
+    const ESAPI_CRYPTO_MODE_LOKI97_ECB = 'ESAPI_CRYPTO_MODE_LOKI97_ECB';
+    const ESAPI_CRYPTO_MODE_LOKI97_NCFB = 'ESAPI_CRYPTO_MODE_LOKI97_NCFB';
+    const ESAPI_CRYPTO_MODE_LOKI97_NOFB = 'ESAPI_CRYPTO_MODE_LOKI97_NOFB';
+    const ESAPI_CRYPTO_MODE_LOKI97_OFB = 'ESAPI_CRYPTO_MODE_LOKI97_OFB';
+    const ESAPI_CRYPTO_MODE_LOKI97_STREAM = 'ESAPI_CRYPTO_MODE_LOKI97_STREAM';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_192_CBC = 'ESAPI_CRYPTO_MODE_RIJNDAEL-192_CBC';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_192_CFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-192_CFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_192_CTR = 'ESAPI_CRYPTO_MODE_RIJNDAEL-192_CTR';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_192_ECB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-192_ECB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_192_NCFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-192_NCFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_192_NOFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-192_NOFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_192_OFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-192_OFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_192_STREAM = 'ESAPI_CRYPTO_MODE_RIJNDAEL-192_STREAM';
+    const ESAPI_CRYPTO_MODE_SAFERPLUS_CBC = 'ESAPI_CRYPTO_MODE_SAFERPLUS_CBC';
+    const ESAPI_CRYPTO_MODE_SAFERPLUS_CFB = 'ESAPI_CRYPTO_MODE_SAFERPLUS_CFB';
+    const ESAPI_CRYPTO_MODE_SAFERPLUS_CTR = 'ESAPI_CRYPTO_MODE_SAFERPLUS_CTR';
+    const ESAPI_CRYPTO_MODE_SAFERPLUS_ECB = 'ESAPI_CRYPTO_MODE_SAFERPLUS_ECB';
+    const ESAPI_CRYPTO_MODE_SAFERPLUS_NCFB = 'ESAPI_CRYPTO_MODE_SAFERPLUS_NCFB';
+    const ESAPI_CRYPTO_MODE_SAFERPLUS_NOFB = 'ESAPI_CRYPTO_MODE_SAFERPLUS_NOFB';
+    const ESAPI_CRYPTO_MODE_SAFERPLUS_OFB = 'ESAPI_CRYPTO_MODE_SAFERPLUS_OFB';
+    const ESAPI_CRYPTO_MODE_SAFERPLUS_STREAM = 'ESAPI_CRYPTO_MODE_SAFERPLUS_STREAM';
+    const ESAPI_CRYPTO_MODE_WAKE_CBC = 'ESAPI_CRYPTO_MODE_WAKE_CBC';
+    const ESAPI_CRYPTO_MODE_WAKE_CFB = 'ESAPI_CRYPTO_MODE_WAKE_CFB';
+    const ESAPI_CRYPTO_MODE_WAKE_CTR = 'ESAPI_CRYPTO_MODE_WAKE_CTR';
+    const ESAPI_CRYPTO_MODE_WAKE_ECB = 'ESAPI_CRYPTO_MODE_WAKE_ECB';
+    const ESAPI_CRYPTO_MODE_WAKE_NCFB = 'ESAPI_CRYPTO_MODE_WAKE_NCFB';
+    const ESAPI_CRYPTO_MODE_WAKE_NOFB = 'ESAPI_CRYPTO_MODE_WAKE_NOFB';
+    const ESAPI_CRYPTO_MODE_WAKE_OFB = 'ESAPI_CRYPTO_MODE_WAKE_OFB';
+    const ESAPI_CRYPTO_MODE_WAKE_STREAM = 'ESAPI_CRYPTO_MODE_WAKE_STREAM';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_COMPAT_CBC = 'ESAPI_CRYPTO_MODE_BLOWFISH-COMPAT_CBC';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_COMPAT_CFB = 'ESAPI_CRYPTO_MODE_BLOWFISH-COMPAT_CFB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_COMPAT_CTR = 'ESAPI_CRYPTO_MODE_BLOWFISH-COMPAT_CTR';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_COMPAT_ECB = 'ESAPI_CRYPTO_MODE_BLOWFISH-COMPAT_ECB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_COMPAT_NCFB = 'ESAPI_CRYPTO_MODE_BLOWFISH-COMPAT_NCFB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_COMPAT_NOFB = 'ESAPI_CRYPTO_MODE_BLOWFISH-COMPAT_NOFB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_COMPAT_OFB = 'ESAPI_CRYPTO_MODE_BLOWFISH-COMPAT_OFB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_COMPAT_STREAM = 'ESAPI_CRYPTO_MODE_BLOWFISH-COMPAT_STREAM';
+    const ESAPI_CRYPTO_MODE_DES_CBC = 'ESAPI_CRYPTO_MODE_DES_CBC';
+    const ESAPI_CRYPTO_MODE_DES_CFB = 'ESAPI_CRYPTO_MODE_DES_CFB';
+    const ESAPI_CRYPTO_MODE_DES_CTR = 'ESAPI_CRYPTO_MODE_DES_CTR';
+    const ESAPI_CRYPTO_MODE_DES_ECB = 'ESAPI_CRYPTO_MODE_DES_ECB';
+    const ESAPI_CRYPTO_MODE_DES_NCFB = 'ESAPI_CRYPTO_MODE_DES_NCFB';
+    const ESAPI_CRYPTO_MODE_DES_NOFB = 'ESAPI_CRYPTO_MODE_DES_NOFB';
+    const ESAPI_CRYPTO_MODE_DES_OFB = 'ESAPI_CRYPTO_MODE_DES_OFB';
+    const ESAPI_CRYPTO_MODE_DES_STREAM = 'ESAPI_CRYPTO_MODE_DES_STREAM';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_256_CBC = 'ESAPI_CRYPTO_MODE_RIJNDAEL-256_CBC';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_256_CFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-256_CFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_256_CTR = 'ESAPI_CRYPTO_MODE_RIJNDAEL-256_CTR';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_256_ECB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-256_ECB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_256_NCFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-256_NCFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_256_NOFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-256_NOFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_256_OFB = 'ESAPI_CRYPTO_MODE_RIJNDAEL-256_OFB';
+    const ESAPI_CRYPTO_MODE_RIJNDAEL_256_STREAM = 'ESAPI_CRYPTO_MODE_RIJNDAEL-256_STREAM';
+    const ESAPI_CRYPTO_MODE_SERPENT_CBC = 'ESAPI_CRYPTO_MODE_SERPENT_CBC';
+    const ESAPI_CRYPTO_MODE_SERPENT_CFB = 'ESAPI_CRYPTO_MODE_SERPENT_CFB';
+    const ESAPI_CRYPTO_MODE_SERPENT_CTR = 'ESAPI_CRYPTO_MODE_SERPENT_CTR';
+    const ESAPI_CRYPTO_MODE_SERPENT_ECB = 'ESAPI_CRYPTO_MODE_SERPENT_ECB';
+    const ESAPI_CRYPTO_MODE_SERPENT_NCFB = 'ESAPI_CRYPTO_MODE_SERPENT_NCFB';
+    const ESAPI_CRYPTO_MODE_SERPENT_NOFB = 'ESAPI_CRYPTO_MODE_SERPENT_NOFB';
+    const ESAPI_CRYPTO_MODE_SERPENT_OFB = 'ESAPI_CRYPTO_MODE_SERPENT_OFB';
+    const ESAPI_CRYPTO_MODE_SERPENT_STREAM = 'ESAPI_CRYPTO_MODE_SERPENT_STREAM';
+    const ESAPI_CRYPTO_MODE_XTEA_CBC = 'ESAPI_CRYPTO_MODE_XTEA_CBC';
+    const ESAPI_CRYPTO_MODE_XTEA_CFB = 'ESAPI_CRYPTO_MODE_XTEA_CFB';
+    const ESAPI_CRYPTO_MODE_XTEA_CTR = 'ESAPI_CRYPTO_MODE_XTEA_CTR';
+    const ESAPI_CRYPTO_MODE_XTEA_ECB = 'ESAPI_CRYPTO_MODE_XTEA_ECB';
+    const ESAPI_CRYPTO_MODE_XTEA_NCFB = 'ESAPI_CRYPTO_MODE_XTEA_NCFB';
+    const ESAPI_CRYPTO_MODE_XTEA_NOFB = 'ESAPI_CRYPTO_MODE_XTEA_NOFB';
+    const ESAPI_CRYPTO_MODE_XTEA_OFB = 'ESAPI_CRYPTO_MODE_XTEA_OFB';
+    const ESAPI_CRYPTO_MODE_XTEA_STREAM = 'ESAPI_CRYPTO_MODE_XTEA_STREAM';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_CBC = 'ESAPI_CRYPTO_MODE_BLOWFISH_CBC';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_CFB = 'ESAPI_CRYPTO_MODE_BLOWFISH_CFB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_CTR = 'ESAPI_CRYPTO_MODE_BLOWFISH_CTR';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_ECB = 'ESAPI_CRYPTO_MODE_BLOWFISH_ECB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_NCFB = 'ESAPI_CRYPTO_MODE_BLOWFISH_NCFB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_NOFB = 'ESAPI_CRYPTO_MODE_BLOWFISH_NOFB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_OFB = 'ESAPI_CRYPTO_MODE_BLOWFISH_OFB';
+    const ESAPI_CRYPTO_MODE_BLOWFISH_STREAM = 'ESAPI_CRYPTO_MODE_BLOWFISH_STREAM';
+    const ESAPI_CRYPTO_MODE_ENIGMA_CBC = 'ESAPI_CRYPTO_MODE_ENIGMA_CBC';
+    const ESAPI_CRYPTO_MODE_ENIGMA_CFB = 'ESAPI_CRYPTO_MODE_ENIGMA_CFB';
+    const ESAPI_CRYPTO_MODE_ENIGMA_CTR = 'ESAPI_CRYPTO_MODE_ENIGMA_CTR';
+    const ESAPI_CRYPTO_MODE_ENIGMA_ECB = 'ESAPI_CRYPTO_MODE_ENIGMA_ECB';
+    const ESAPI_CRYPTO_MODE_ENIGMA_NCFB = 'ESAPI_CRYPTO_MODE_ENIGMA_NCFB';
+    const ESAPI_CRYPTO_MODE_ENIGMA_NOFB = 'ESAPI_CRYPTO_MODE_ENIGMA_NOFB';
+    const ESAPI_CRYPTO_MODE_ENIGMA_OFB = 'ESAPI_CRYPTO_MODE_ENIGMA_OFB';
+    const ESAPI_CRYPTO_MODE_ENIGMA_STREAM = 'ESAPI_CRYPTO_MODE_ENIGMA_STREAM';
+    const ESAPI_CRYPTO_MODE_RC2_CBC = 'ESAPI_CRYPTO_MODE_RC2_CBC';
+    const ESAPI_CRYPTO_MODE_RC2_CFB = 'ESAPI_CRYPTO_MODE_RC2_CFB';
+    const ESAPI_CRYPTO_MODE_RC2_CTR = 'ESAPI_CRYPTO_MODE_RC2_CTR';
+    const ESAPI_CRYPTO_MODE_RC2_ECB = 'ESAPI_CRYPTO_MODE_RC2_ECB';
+    const ESAPI_CRYPTO_MODE_RC2_NCFB = 'ESAPI_CRYPTO_MODE_RC2_NCFB';
+    const ESAPI_CRYPTO_MODE_RC2_NOFB = 'ESAPI_CRYPTO_MODE_RC2_NOFB';
+    const ESAPI_CRYPTO_MODE_RC2_OFB = 'ESAPI_CRYPTO_MODE_RC2_OFB';
+    const ESAPI_CRYPTO_MODE_RC2_STREAM = 'ESAPI_CRYPTO_MODE_RC2_STREAM';
+    const ESAPI_CRYPTO_MODE_TRIPLEDES_CBC = 'ESAPI_CRYPTO_MODE_TRIPLEDES_CBC';
+    const ESAPI_CRYPTO_MODE_TRIPLEDES_CFB = 'ESAPI_CRYPTO_MODE_TRIPLEDES_CFB';
+    const ESAPI_CRYPTO_MODE_TRIPLEDES_CTR = 'ESAPI_CRYPTO_MODE_TRIPLEDES_CTR';
+    const ESAPI_CRYPTO_MODE_TRIPLEDES_ECB = 'ESAPI_CRYPTO_MODE_TRIPLEDES_ECB';
+    const ESAPI_CRYPTO_MODE_TRIPLEDES_NCFB = 'ESAPI_CRYPTO_MODE_TRIPLEDES_NCFB';
+    const ESAPI_CRYPTO_MODE_TRIPLEDES_NOFB = 'ESAPI_CRYPTO_MODE_TRIPLEDES_NOFB';
+    const ESAPI_CRYPTO_MODE_TRIPLEDES_OFB = 'ESAPI_CRYPTO_MODE_TRIPLEDES_OFB';
+    const ESAPI_CRYPTO_MODE_TRIPLEDES_STREAM = 'ESAPI_CRYPTO_MODE_TRIPLEDES_STREAM';
+    
+    /**
+    * @param string	$operation 
+    * 			the operation to execute
+    * @param string	$data 
+    * 			the plaintext string to encrypt/decrypt/hash
+    * 
+    * @return string the encrypted/hashed string representation of 'plaintext'
+    * 
+    * @throws throw an exception if the operation does not exist
+    */
+    public function getCrypto($operation, $data);
+    
+    /**
+    * Gets an absolute timestamp representing an offset from the current time to be used by
+    * other functions in the library.
+    * 
+    * @param int $offset 
+    * 			the offset to add to the current time
+    * 
+    * @return 
+    * 			the absolute timestamp
+    */
+    function getRelativeTimeStamp($offset);
 
-	/**
-	 * Returns a string representation of the hash of the provided plaintext and
-	 * salt. The salt helps to protect against a rainbow table attack by mixing
-	 * in some extra data with the plaintext. Some good choices for a salt might
-	 * be an account name or some other string that is known to the application
-	 * but not to an attacker. 
-	 * See <a href="http://www.matasano.com/log/958/enough-with-the-rainbow-tables-what-you-need-to-know-about-secure-password-schemes/">
-	 * this article</a> for more information about hashing as it pertains to password schemes.
-	 * 
-	 * @param plaintext
-	 * 		the plaintext String to encrypt
-	 * @param salt
-	 *      the salt to add to the plaintext String before hashing
-	 * 
-	 * @return 
-	 * 		the encrypted hash of 'plaintext' stored as a String
-	 * 
-	 * @throws EncryptionException
-	 *      if the specified hash algorithm could not be found or another problem exists with 
-	 *      the hashing of 'plaintext'
-	 */
-	function hash($plaintext, $salt);
 
-	/**
-	 * Encrypts the provided plaintext and returns a ciphertext string.
-	 * 
-	 * @param plaintext
-	 *      the plaintext String to encrypt
-	 * 
-	 * @return 
-	 * 		the encrypted String representation of 'plaintext'
-	 * 
-	 * @throws EncryptionException
-	 *      if the specified encryption algorithm could not be found or another problem exists with 
-	 *      the encryption of 'plaintext'
-	 */
-	function encrypt($plaintext);
+    /**
+    * Gets a timestamp representing the current date and time to be used by
+    * other functions in the library.
+    * 
+    * @return 
+    *              a timestamp representing the current time
+    */
+    function getTimeStamp();   
 
-	/**
-	 * Decrypts the provided ciphertext string (encrypted with the encrypt
-	 * method) and returns a plaintext string.
-	 * 
-	 * @param ciphertext
-	 *      the ciphertext (encrypted plaintext)
-	 * 
-	 * @return 
-	 * 		the decrypted ciphertext
-	 * 
-	 * @throws EncryptionException
-	 *      if the specified encryption algorithm could not be found or another problem exists with 
-	 *      the encryption of 'plaintext'
-	 */
-	function decrypt($ciphertext);
-
-	/**
-	 * Create a digital signature for the provided data and return it in a
-	 * string.
-	 * 
-	 * @param data
-	 *      the data to sign
-	 * 
-	 * @return 
-	 * 		the digital signature stored as a String
-	 * 
-	 * @throws EncryptionException
-	 * 		if the specified signature algorithm cannot be found
-	 */
-	function sign($data);
-
-	/**
-	 * Verifies a digital signature (created with the sign method) and returns
-	 * the boolean result.
-	 * 
-	 * @param signature
-	 *      the signature to verify against 'data'
-	 * @param data
-	 *      the data to verify against 'signature'
-	 * 
-	 * @return 
-	 * 		true, if the signature is verified, false otherwise
-	 * 
-	 */
-	function verifySignature($signature, $data);
-
-	/**
-	 * Creates a seal that binds a set of data and includes an expiration timestamp.
-	 * 
-	 * @param data
-	 *      the data to seal
-	 * @param timestamp
-	 *      the absolute expiration date of the data, expressed as seconds since the epoch
-	 * 
-	 * @return 
-	 * 		the seal
-	 * 
-	 */
-	function seal($data, $timestamp);
-
-	/**
-	 * Unseals data (created with the seal method) and throws an exception
-	 * describing any of the various problems that could exist with a seal, such
-	 * as an invalid seal format, expired timestamp, or decryption error.
-	 * 
-	 * @param seal
-	 *      the sealed data
-	 * 
-	 * @return 
-	 * 		the original (unsealed) data
-	 * 
-	 * @throws EncryptionException 
-	 * 		if the unsealed data cannot be retrieved for any reason
-	 */
-	function unseal($seal);
-	
-	/**
-	 * Verifies a seal (created with the seal method) and throws an exception
-	 * describing any of the various problems that could exist with a seal, such
-	 * as an invalid seal format, expired timestamp, or data mismatch.
-	 * 
-	 * @param seal
-	 *      the seal to verify
-	 * 
-	 * @return 
-	 * 		true, if the seal is valid.  False otherwise
-	 */
-	function verifySeal($seal);
-	
-	/**
-	 * Gets an absolute timestamp representing an offset from the current time to be used by
-	 * other functions in the library.
-	 * 
-	 * @param offset 
-	 * 		the offset to add to the current time
-	 * 
-	 * @return 
-	 * 		the absolute timestamp
-	 */
-	function getRelativeTimeStamp($offset);
-	
-	
-	/**
-	 * Gets a timestamp representing the current date and time to be used by
-	 * other functions in the library.
-	 * 
-	 * @return 
-	 * 		a timestamp representing the current time
-	 */
-	function getTimeStamp();
 }
 ?>
--- trunk/test/reference/EncryptorTest.php
+++ trunk/test/reference/EncryptorTest.php
@@ -8,27 +8,160 @@
  *
  * Copyright (c) 2007 - 2009 The OWASP Foundation
  * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
+ * The ESAPI is published by OWASP under the BSD license. You should read 
+ * and accept the LICENSE before you use, modify, and/or redistribute this software.
  * 
- * @author Andrew van der Stock (vanderaj @ owasp.org)
+ * @author Arnaud Labenne - dotsafe.fr
  * @created 2009
  */
-class EncryptorTest extends UnitTestCase 
+
+require_once dirname(__FILE__).'/../../src/ESAPI.php';
+
+class EncryptorTest extends UnitTestCase
 {
-	function setUp() 
-	{
-		
-	}
-	
-	function tearDown()
-	{
-		
-	}
-	
-	function testFail()
-	{
-		$this->fail();
-	}
+    function setUp()
+    {
+        global $ESAPI;
+
+        if ( !isset($ESAPI)) {
+            $ESAPI = new ESAPI();
+        }
+        
+        $this->encryptor = $ESAPI->getEncryptor();
+    }
+    
+    function tearDown()
+    {
+        
+    }
+    
+    function testHashSHA1()
+    {
+        $this->assertEqual(
+            "MmYzM2M2NGI3MGM5YmZjYjhjOGI2NTk4ZDNkODRlMDg5OWVmMDQ4Ng==", 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_HASH_ASCII_HEX, 
+                "OWASP ESAPI for PHP", 
+                "Salt is my friend !"
+            )
+        );
+    }
+    
+    function testHashNull()
+    {
+        $this->assertEqual(
+            null, 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_HASH_ASCII_HEX, 
+                null                
+            )
+        );
+    }
+    
+    function testHashEmpty()
+    {
+        $this->assertEqual(
+            null, 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_HASH_ASCII_HEX, 
+                "", 
+                "Salt is my friend !"
+            )
+        );
+    }
+    
+    function testHashNoSaltSHA1()
+    {
+        $this->assertEqual(
+            "MzcwZmUxMWExM2ZlNTkwOGYwMjlkNjljMjhhMDY1NzIyZTVlOTI4MA==", 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_HASH_ASCII_HEX, 
+                "OWASP ESAPI for PHP"
+            )
+        );
+    }
+    
+    function testEncryptAES256ECB()
+    {
+        $this->assertEqual(
+            "XglezBjL4kLRg9xKKIUC6C0nMucKj3LFOyQbnaQr1FA=", 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_ENCRYPT_ASCII_HEX, 
+                "OWASP ESAPI for PHP"
+            )
+        );
+    }
+    
+    function testEncryptNull()
+    {
+        $this->assertEqual(
+            null, 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_ENCRYPT_ASCII_HEX, 
+                null
+            )
+        );
+    }
+    
+    function testEncryptEmpty()
+    {
+        $this->assertEqual(
+            null, 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_ENCRYPT_ASCII_HEX, 
+                ""
+            )
+        );
+    }
+    
+    function testDecryptNull()
+    {
+        $this->assertEqual(
+            null, 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_DECRYPT_ASCII_HEX, 
+                null
+            )
+        );
+    }
+    
+    function testDecryptEmpty()
+    {
+        $this->assertEqual(
+            null, 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_DECRYPT_ASCII_HEX, 
+                ""
+            )
+        );
+    }
+    
+    function testDecryptAES256ECB()
+    {
+        $this->assertEqual(
+            "OWASP ESAPI for PHP", 
+            ESAPI::getEncryptor()->getCrypto(
+                Encryptor::ESAPI_CRYPTO_OP_DECRYPT_ASCII_HEX, 
+                "XglezBjL4kLRg9xKKIUC6C0nMucKj3LFOyQbnaQr1FA="
+            )
+        );
+    }
+
+    function testEncryptDecrypt()
+    {
+        $secret = "OWASP ESAPI for PHP";
+
+        $encryptedValue = ESAPI::getEncryptor()->getCrypto(
+            Encryptor::ESAPI_CRYPTO_OP_ENCRYPT_ASCII_HEX, 
+            $secret
+        );
+
+        $decryptedValue = ESAPI::getEncryptor()->getCrypto(
+            Encryptor::ESAPI_CRYPTO_OP_DECRYPT_ASCII_HEX, 
+            $encryptedValue
+        );
+
+        $this->assertEqual($secret, $decryptedValue);        
+    }
 }
 ?>
