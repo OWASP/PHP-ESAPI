e73f29b81fb69d15338bf2715572386746321db4
jahboite <jahboite@gmail.com>
2010-03-13 22:14:22 +0000

    Completed (for now) IntrusionDetector.

--- src/IntrusionDetector.php
+++ src/IntrusionDetector.php
@@ -1,106 +1,84 @@
 <?php
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
- * This file is part of the Open Web Application Security Project (OWASP)
- * Enterprise Security API (ESAPI) project. For details, please see
- * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - 2009 The OWASP Foundation
- * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
- * 
- * @author jah (at jahboite.co.uk)
- * @created 2008
- * @since 1.4
- * @package org.owasp.esapi
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @author    Jeff Williams <jeff.williams@owasp.org>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
-
-require_once dirname(__FILE__).'/errors/IntrusionException.php';
 
 
 /**
- * The IntrusionDetector interface is intended to track security relevant events and identify attack behavior. The
- * implementation can use as much state as necessary to detect attacks, but note that storing too much state will burden
+ * Implementations require IntrusionException.
+ */
+require_once dirname(__FILE__) . '/errors/IntrusionException.php';
+
+
+/**
+ * The IntrusionDetector interface is intended to track security relevant events
+ * and identify attack behavior. The implementation can use as much state as
+ * necessary to detect attacks, but note that storing too much state will burden
  * your system.
- * <P>
+ *
  * <img src="doc-files/IntrusionDetector.jpg">
- * <P>
- * <P>
- * The interface is currently designed to accept exceptions as well as custom events. Implementations can use this
- * stream of information to detect both normal and abnormal behavior.
- * 
- * @author 
- * @since 1.4
+ *
+ * The interface is currently designed to accept exceptions as well as custom
+ * events. Implementations can use this stream of information to detect both
+ * normal and abnormal behavior.
+ *
+ * PHP version 5.2.9
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @version   1.0
+ * @author    Jeff Williams <jeff.williams@owasp.org>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 interface IntrusionDetector {
 
     /**
-     * Adds the exception to the IntrusionDetector.  This method should immediately log the exception so that developers throwing an 
-     * IntrusionException do not have to remember to log every error.  The implementation should store the exception somewhere for the current user
-     * in order to check if the User has reached the threshold for any Enterprise Security Exceptions.  The User object is the recommended location for storing
-     * the current user's security exceptions.  If the User has reached any security thresholds, the appropriate security action can be taken and logged.
+     * Adds an exception to the IntrusionDetector.
      * 
-     * @param exception 
-     * 		the exception thrown
-     * 
-     * @throws IntrusionException 
-     * 		the intrusion exception
+     * This method should immediately log the exception so that developers
+     * throwing an IntrusionException do not have to remember to log every
+     * error.  The implementation should store the exception somewhere for the
+     * current user in order to check if the User has reached the threshold for
+     * any Enterprise Security Exceptions.  The User object is the recommended
+     * location for storing the current user's security exceptions.  If the User
+     * has reached any security thresholds, the appropriate security action can
+     * be taken and logged.
+     *
+     * @param $exception string exception thrown.
      */
     function addException($exception);
 
+
     /**
-     * Adds the event to the IntrusionDetector.  This method should immediately log the event.  The implementation should store the event somewhere for the current user
-     * in order to check if the User has reached the threshold for any Enterprise Security Exceptions.  The User object is the recommended location for storing
-     * the current user's security event.  If the User has reached any security thresholds, the appropriate security action can be taken and logged.
+     * Adds an event to the IntrusionDetector.
      * 
-     * @param eventName 
-     * 		the event to add
-     * @param logMessage 
-     * 		the message to log with the event
-     * 
-     * @throws IntrusionException 
-     * 		the intrusion exception
+     * This method should immediately log the event.  The implementation should
+     * store the event somewhere for the current user in order to check if the
+     * User has reached the threshold for any Enterprise Security Exceptions.
+     * The User object is the recommended location for storing the current
+     * user's security event.  If the User has reached any security thresholds,
+     * the appropriate security action can be taken and logged.
+     *
+     * @param $eventName string event to add.
+     * @param $logMessage string message to log with the event.
      */
     function addEvent($eventName, $logMessage);
 
-}
-
-/*
- * Intrusion Event.
- */
-class Event
-{
-
-	public $key;
-	public $times = array();
-	public $count = 0;
-	
-	public function __construct($key)
-	{
-		$this->key = $key;
-	}
-	
-	public function increment($count, $interval)
-	{
-		$now = time();
-		array_unshift($this->times, $now );
-		$this->count++;
-		
-		while ( sizeof($this->times) > $count )
-		{
-			array_pop($this->times);
-		}
-		if ( sizeof($this->times) == $count )
-		{
-			$plong = end($this->times);
-			$nlong = time();
-			if ( $nlong - $plong > $interval )
-			{
-				throw new IntrusionException( "Threshold exceeded", "Exceeded threshold for " . $this->key );
-			}
-		}
-	}
 
 }
--- test/reference/IntrusionDetectorTest.php
+++ test/reference/IntrusionDetectorTest.php
@@ -1,92 +1,265 @@
 <?php
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
- * This file is part of the Open Web Application Security Project (OWASP)
- * Enterprise Security API (ESAPI) project. For details, please see
- * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - 2009 The OWASP Foundation
- * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
- * 
- * @author jah (at jahboite.co.uk)
- * @created 2009
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 
-require_once dirname(__FILE__) . '/../http/TestHttpServletRequest.php';
-require_once dirname(__FILE__) . '/../http/TestHttpServletResponse.php';
 
-class IntrusionDetectorTest extends UnitTestCase 
+/**
+ * Test for the DefaultIntrusionDetector implementation of the IntrusionDetector
+ * interface.
+ *
+ * PHP version 5.2.9
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @version   1.0
+ * @author    jah <jah@jahboite.co.uk>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+class IntrusionDetectorTest extends UnitTestCase
 {
-	// FIXME: remove the next 1 line when DefaultEncoder::CHAR_ALPHANUMERICS is avalable
-	private $CHAR_ALPHANUMERICS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890';	
-	
-	function setUp() 
-	{
-		global $ESAPI;
-		if (!isset($ESAPI)) 
-		{
-			$ESAPI = new ESAPI(dirname(__FILE__).'/../testresources/ESAPI.xml');
-		}
-	}
-	
-	function tearDown() {}
-	
-	/**
-	 * Test of addException method, of class org.owasp.esapi.IntrusionDetector.
-	 * 
-	 * @throws AuthenticationException
-	 *             the authentication exception
-	 */
-	function testAddException()
-	{
-		$this->fail(); /*
-		ESAPI::getIntrusionDetector()->addException(new IntrusionException("user message", "log message"));
-		$username = ESAPI::getRandomizer()->getRandomString(8, $this->CHAR_ALPHANUMERICS); // FIXME: DefaultEncoder::CHAR_ALPHANUMERICS
-		$auth = ESAPI::getAuthenticator();
-		$user = $auth->createUser($username, 'addException', 'addException');
-		$user->enable();
-		$request  = null; // FIXME: new TestHttpServletRequest();
-		$response = null; // FIXME: new TestHttpServletResponse();
-		ESAPI::getHttpUtilities()->setCurrentHTTP($request, $response);
-		$user->loginWithPassword('addException');
-			
-		// Now generate some exceptions to disable account
-		for ( $i = 0; $i < ESAPI::getSecurityConfiguration()->getQuota('IntegrityException')->count; $i++ )
-		{
-			// EnterpriseSecurityExceptions are added to IntrusionDetector automatically
-			new IntegrityException("IntegrityException " + $i, "IntegrityException " + $i);
-		}
-		$this->assertFalse($user->isLoggedIn());
-		*/
-	}
-	
-	/**
-	 * Test of addEvent method, of class org.owasp.esapi.IntrusionDetector.
-	 *
-	 * @throws AuthenticationException
-	 *             the authentication exception
-	 */
+
+    private $logFileLoc = null;
+    private $charset = null;
+    private $rnd = null;
+
+    function setUp()
+    {
+        global $ESAPI;
+        if (!isset($ESAPI))
+        {
+            $ESAPI = new ESAPI(dirname(__FILE__).'/../testresources/ESAPI.xml');
+        }
+    }
+
+    function tearDown() {}
+
+    /**
+     * Test to ensure that EnterpriseSecurityExceptions are automatically added
+     * to the IntrusionDetector and that the IntrusionDetector logs the
+     * exceptions logMessage.
+     *
+     */
+    function testExceptionAutoAdd()
+    {
+        $this->getLogFileLoc();
+        if ($this->logFileLoc === false) {
+            $this->fail(
+                'Cannot perform this test because the log file cannot be found.'
+            );
+        }
+
+        $logMsg = 'testExceptionAutoAdd:';
+        $logMsg .= $this->getRandomAlphaNumString(32);
+        new EnterpriseSecurityException(
+            'user message: testExceptionAutoAdd', $logMsg
+        );
+
+        $m = 'Test attempts to detect exception log message in logfile: %s';
+        $this->assertTrue(
+            $this->fileContainsExpected($this->logFileLoc, $logMsg),
+            $m
+        );
+    }
+
+
+    /**
+     * Test of addException method of class DefaultIntrusionDetector.
+     *
+     */
+    function testAddException()
+    {
+        $this->getLogFileLoc();
+        if ($this->logFileLoc === false) {
+            $this->fail(
+                'Cannot perform this test because the log file cannot be found.'
+            );
+        }
+
+        $logMsg = 'testAddException:';
+        $logMsg .= $this->getRandomAlphaNumString(32);
+        ESAPI::getIntrusionDetector()->addException(new Exception($logMsg));
+
+        $m = 'Test attempts to detect exception log message in logfile: %s';
+        $this->assertTrue(
+            $this->fileContainsExpected($this->logFileLoc, $logMsg),
+            $m
+        );
+    }
+
+
+    /**
+     * Test of addEvent method of DefaultIntrusionDetector.  This test checks
+     * that a threshold exceeded message is logged and thus tests the addEvent,
+     * addSecurityEvent and Event.increment methods and that takeSecurityAction
+     * performs the 'log' action.
+     *
+     */
     function testAddEvent()
     {
-        $this->fail(); /*
-        $username = ESAPI::getRandomizer()->getRandomString(8, $this->CHAR_ALPHANUMERICS); // FIXME: DefaultEncoder::CHAR_ALPHANUMERICS
-        $auth = ESAPI::getAuthenticator();
-		$user = $auth->createUser($username, 'addEvent', 'addEvent');
-		$user->enable();
-		$request  = null; // FIXME: new TestHttpServletRequest();
-		$response = null; // FIXME: new TestHttpServletResponse();
-		ESAPI::getHttpUtilities()->setCurrentHTTP($request, $response);
-		$user->loginWithPassword('addEvent');
-        
-        // Now generate some events to disable user account
-        for ( $i = 0; $i < ESAPI::getSecurityConfiguration()->getQuota('test')->count; $i++ )
-        {
-            ESAPI::getIntrusionDetector()->addEvent("test", "test message");
+        // generate a random event name
+        $eventName = $this->getRandomAlphaNumString(16);
+
+        // create a threshold for this event so that it will be tracked by IDS
+        require_once dirname(__FILE__) . '/../../src/SecurityConfiguration.php';
+        $threshold = new Threshold($eventName, 1, 60, array('log'));
+        $secConfig = ESAPI::getSecurityConfiguration();
+        $secConfig->getQuota('1'); // make sure events are loaded
+        $secConfig->events[] = $threshold;
+
+        // add event
+        ESAPI::getIntrusionDetector()->addEvent(
+            $eventName,
+            'This is a Test Event for IntrusionDetectorTest.'
+        );
+
+        $find = 'User exceeded quota of 1 per 60 seconds for event ' .
+            $eventName . '. Taking actions \[log\]';
+        $m = 'Test attempts to detect IntrusionDetector' .
+            ' action log message in logfile: %s';
+        $this->assertTrue(
+            $this->fileContainsExpected($this->logFileLoc, $find),
+            $m
+        );
+    }
+
+
+    /**
+     * This test shows that IntrusionExceptions can be tracked by
+     * IntrusionDetector.
+     *
+     */
+    function testAddIntrusionExceptionIsTracked()
+    {
+        $this->getLogFileLoc();
+        if ($this->logFileLoc === false) {
+            $this->fail(
+                'Cannot perform this test because the log file cannot be found.'
+            );
         }
-        $this->assertFalse($user->isEnabled()); */
+
+        // modify the threshold for this event so that it can be reliably
+        // detected in the log file.
+        require_once dirname(__FILE__) . '/../../src/SecurityConfiguration.php';
+        $dummyAction = 'TEST' . $this->getRandomAlphaNumString(16);
+        $moddedThreshold = new Threshold(
+            'IntrusionException', 1, 1, array('log', $dummyAction)
+        );
+        $secConfig = ESAPI::getSecurityConfiguration();
+        $secConfig->getQuota('1'); // make sure events are loaded
+        $ieKey = null;
+        $restoreThreshold = null;
+        foreach ($secConfig->events as $key => $threshold) {
+            if ($threshold->name == 'IntrusionException') {
+                $ieKey = $key;
+                $restoreThreshold = $threshold;
+                $secConfig->events[$key] = $moddedThreshold;
+                break;
+            }
+        }
+
+        ESAPI::getIntrusionDetector()->addException(
+            new IntrusionException(
+                'Naughty User!',
+                'testAddIntrusionExceptionIsTracked'
+            )
+        );
+
+        // restore the threshold actions
+        $secConfig->events[$ieKey] = $restoreThreshold;
+        
+
+        $find = 'User exceeded quota of 1 per 1 seconds for event ' .
+            "IntrusionException. Taking actions \[log, {$dummyAction}\]";
+        $m = 'Test attempts to detect IntrusionDetector' .
+            ' action log message in logfile: %s';
+        $this->assertTrue(
+            $this->fileContainsExpected($this->logFileLoc, $find),
+            $m
+        );
+    }
+
+
+    /**
+     * Helper method which opens a file handle to the supplied path, reads it
+     * line-by-line and performs preg_match on the line with the supplied regex.
+     *
+     * @param  $filename string path to file.
+     * @param  $expected string regex to match in the file
+     *
+     * @return true if the regex matches, false if not, null if the file
+     *         cannot be opened.
+     */
+    private function fileContainsExpected($filename, $expected)
+    {
+        if (empty($filename) || ! is_string($filename)) {
+            return null;
+        }
+        $f = fopen($filename, 'r');
+        if ($f === false) {
+            return null;
+        }
+        while (! feof($f)) {
+            $line = fgets($f, 2048);
+            if (preg_match("/{$expected}/", $line)) {
+                fclose($f);
+                return true;
+            }
+        }
+        fclose($f);
+        return false;
+    }
+
+
+    /**
+     * Helper method sets $this->logFileLoc with the ESAPILogger log file path.
+     */
+    private function getLogFileLoc()
+    {
+        if ($this->logFileLoc !== null) {
+            return;
+        }
+        $filename = ESAPI::getSecurityConfiguration()->getLogFileName();
+        $this->logFileLoc = realpath($filename);
+    }
+
+
+    /**
+     * Helper method returns a random string of alphanumeric characters of the
+     * supplied length.
+     *
+     * @param  $len integer length of the required string.
+     *
+     * @return string of $len alphanumeric characters.
+     */
+    private function getRandomAlphaNumString($len)
+    {
+        if ($this->charset === null) {
+            ESAPI::getEncoder();
+            $this->charset = Encoder::CHAR_ALPHANUMERICS;
+        }
+        if ($this->rnd === null) {
+            $this->rnd = ESAPI::getRandomizer();
+        }
+        if (empty($len)) {
+            return null;
+        }
+        return $this->rnd->getRandomString($len, $this->charset);
     }
 }
-?>
