1ef0898584f1ecc7b597d1050ca53b400ad974b0
coreform@gmail.com <coreform@gmail.com@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-03-16 06:01:24 +0000

    Added Executor tests for Windows. Created executeSystemCommand_effectLonghand, which emulates the JAVA version's overloaded executeSystemCommand method. Minor logic adjustments to DefaultExecutor.

--- trunk/src/Executor.php
+++ trunk/src/Executor.php
@@ -48,6 +48,7 @@ require_once dirname(__FILE__) . '/errors/ExecutorException.php';
  * @author    Jeff Williams <jeff.williams@aspectsecurity.com>
  * @author    Andrew van der Stock <vanderaj@owasp.org>
  * @author    Mike Boberski <boberski_michael@bah.com>
+ * @author    Linden Darling <linden.darling@jds.net.au>
  * @copyright 2009-2010 The OWASP Foundation
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
  * @link      http://www.owasp.org/index.php/ESAPI
@@ -56,20 +57,41 @@ interface Executor
 {
 
     /**
-     * Executes a system command after checking that the executable exists and
-     * escaping all the parameters to ensure that injection is impossible.
-     * Implementations must change to the specified working
-     * directory before invoking the command.
-     * 
-     * @param executable
-     *            the command to execute
-     * @param params
-     *            the parameters of the command being executed
-     * @return the output of the command being run
-     * 
-     * @throws ExecutorException
-     *             the service exception
-     */
-    function executeSystemCommand($executable, $params);
+         * Invokes the specified executable with default workdir and not logging parameters.
+         * 
+         * @param executable
+         *            the command to execute
+         * @param params
+         *            the parameters of the command being executed
+         */
+        function executeSystemCommand($executable, $params);
+
+    /**
+         * Executes a system command after checking that the executable exists and
+         * escaping all the parameters to ensure that injection is impossible.
+         * Implementations must change to the specified working
+         * directory before invoking the command.
+         * 
+         * @param executable
+         *            the command to execute
+         * @param params
+         *            the parameters of the command being executed
+         * @param workdir
+         *            the working directory
+         * @param codec
+         *            the codec to use to encode for the particular OS in use
+         * @param logParams
+         *            use false if any parameters contains sensitive or confidential information.
+         *            (this is an ESAPI 2.0 feature)
+         * 
+         * @return the output of the command being run
+         * 
+         * @throws ExecutorException
+         *             the service exception
+         *             
+         * note: this is PHP's equivalent to ESAPI4JAVA's overloaded executeSystemCommand($executable, $params, $workdir, $codec, $logParams)
+         * note: the codec argument has been eliminated from this implementation since PHP's escapeshellcmd function does enough to not require explicit OS codecs
+         */
+        function executeSystemCommand_effectLonghand($executable, $params, $workdir, $logParams);
 
 }
--- trunk/test/reference/ExecutorTest.php
+++ trunk/test/reference/ExecutorTest.php
@@ -11,7 +11,8 @@
  * The ESAPI is published by OWASP under the BSD license. You should read and accept the
  * LICENSE before you use, modify, and/or redistribute this software.
  * 
- * @author Andrew van der Stock (vanderaj @ owasp.org)
+ * @author  Andrew van der Stock <vanderaj @ owasp.org>
+ * @author  Linden Darling <linden.darling@jds.net.au> 
  * @created 2009
  */
 require_once dirname(__FILE__).'/../../src/ESAPI.php';
@@ -39,19 +40,109 @@ class ExecutorTest extends UnitTestCase
 	*/
 	function testExecuteWindowsSystemCommand() 
     {   	
-        if(substr(PHP_OS, 0, 3) != 'WIN') {
+    	if(substr(PHP_OS, 0, 3) != 'WIN')
+        {
+        	echo "testExecuteWindowsSystemCommand - on non-Windows platform, exiting<br />\n";
         	return; // Not windows, not going to execute this path
         }
         
-        try {
-	    	$instance =  new DefaultExecutor();
+        $instance =  new DefaultExecutor();
+        $codec = new WindowsCodec();
+        
+        try
+        {
+        	$executable = '%SYSTEMROOT%\\System32\\cmd.exe';
 	    	$params = array("/C", "dir");
-	    	$result = $instance->executeSystemCommand("%SYSTEMROOT%\\System32\\cmd.exe", $params);
+	    	$result = $instance->executeSystemCommand($executable, $params);
 	    	$result = ESAPI::getEncoder()->encodeForHTML($result);
-//	    	echo "<br />RESULT: $result<br />\n";
+	    	echo "RESULT: $result<br /><br />\n";
 	    	$this->assertNotNull($result);
         }
     	catch ( Exception $e ) 
+        {
+        	$this->fail();
+        }
+        
+        try
+        {
+        	$executable = '%SYSTEMROOT%\\System32\\;notepad.exe';
+        	$params = array("/C", "dir");
+        	$result= $instance->executeSystemCommand($executable, $params);
+	    	$result = ESAPI::getEncoder()->encodeForHTML($result);
+	    	echo "RESULT: $result<br /><br />\n";
+	    	$this->assertNull($result);
+        }
+        catch( Exception $e )
+        {
+        	//expected
+        	$this->pass();
+        }
+        
+        try
+        {
+        	$executable = '%SYSTEMROOT%\\System32\\..\\cmd.exe';
+        	$params = array("/C", "dir");
+        	$result= $instance->executeSystemCommand($executable, $params);
+	    	$result = ESAPI::getEncoder()->encodeForHTML($result);
+	    	echo "RESULT: $result<br /><br />\n";
+	    	$this->assertNull($result);
+        }
+        catch( Exception $e )
+        {
+        	//expected
+        	$this->pass();
+        }
+        
+    	try
+        {
+        	$executable = '%SYSTEMROOT%\\System32\\cmd.exe';
+        	$params = array("/C", "dir");
+        	$workdir = 'C:\\ridiculous';
+        	$result = $instance->executeSystemCommand_effectLonghand($executable, $params, $workdir, $codec, false);
+	    	$result = ESAPI::getEncoder()->encodeForHTML($result);
+	    	echo "RESULT: $result<br /><br />\n";
+	    	$this->assertNull($result);
+        }
+        catch( Exception $e )
+        {
+        	//expected
+        }
+        
+    	try
+        {
+        	$params = array("/C", "dir", "&dir");
+        	$result= $instance->executeSystemCommand($executable, $params);
+	    	$result = ESAPI::getEncoder()->encodeForHTML($result);
+	    	echo "RESULT: $result<br /><br />\n";
+	    	$this->assertNotNull($result);
+        }
+        catch( Exception $e )
+        {
+        	$this->fail();
+        }
+        
+    	try
+        {
+        	$params = array("/C", "dir", "c:\\autoexec.bat");
+        	$result= $instance->executeSystemCommand($executable, $params);
+	    	$result = ESAPI::getEncoder()->encodeForHTML($result);
+	    	echo "RESULT: $result<br /><br />\n";
+	    	$this->assertNotNull($result);
+        }
+        catch( Exception $e )
+        {
+        	$this->fail();
+        }
+        
+    	try
+        {
+        	$params = array("/C", "dir", "c:\\autoexec.bat c:\\config.sys");
+        	$result= $instance->executeSystemCommand($executable, $params);
+	    	$result = ESAPI::getEncoder()->encodeForHTML($result);
+	    	echo "RESULT: $result<br /><br />\n";
+	    	$this->assertNotNull($result);
+        }
+        catch( Exception $e )
         {
         	$this->fail();
         }
@@ -70,9 +161,58 @@ class ExecutorTest extends UnitTestCase
         	return;
         }
         
-        //TODO: add tests
-        	
         $this->fail();
-    }	
+        
+        //TODO: convert JAVA tests to PHP
+        /*
+        // make sure we have what /bin/sh is pointing at in the allowed exes for the test
+                // and a usable working dir
+                File binSh = new File("/bin/sh").getCanonicalFile();
+                ESAPI.setSecurityConfiguration(
+                        new Conf(
+                                ESAPI.securityConfiguration(),
+                                Collections.singletonList(binSh.getPath()),
+                                new File("/tmp")
+                        )
+                );
+
+                Executor instance = ESAPI.executor();
+                File executable = binSh;
+                List params = new ArrayList();
+                try {
+                        params.add("-c");
+                        params.add("ls");
+                        params.add("/");
+                        String result = instance.executeSystemCommand(executable, new ArrayList(params) );
+                        System.out.println( "RESULT: " + result );
+                        assertTrue(result.length() > 0);
+                } catch (Exception e) {
+                        fail(e.getMessage());
+                }
+                try {
+                        File exec2 = new File( executable.getPath() + ";./inject" );
+                        String result = instance.executeSystemCommand(exec2, new ArrayList(params) );
+                        System.out.println( "RESULT: " + result );
+                        fail();
+                } catch (Exception e) {
+                        // expected
+                }
+                try {
+                        File exec2 = new File( executable.getPath() + "/../bin/sh" );
+                        String result = instance.executeSystemCommand(exec2, new ArrayList(params) );
+                        System.out.println( "RESULT: " + result );
+                        fail();
+                } catch (Exception e) {
+                        // expected
+                }
+                try {
+                        params.add(";ls");
+                        String result = instance.executeSystemCommand(executable, new ArrayList(params) );
+                        System.out.println( "RESULT: " + result );
+                } catch (Exception e) {
+                        fail();
+                }
+        */
+    }
 }
 ?>
