ec5e89e9000d62366e9f2e20b4e7d697bf531bb1
jahboite <jahboite@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-03-27 02:48:13 +0000

    Added Sanitizer Interface which defines one method: getSanitizedHTML Added DefaultSanitizer implementation and SanitizerTest. Changed all getValidXXX methods to assertValidXXX in DefaultValidator and ensured consistent behaviour of all Validator methods. Ensured correct (if a little brief) documentation for all Validator methods. Implemented a few of the remaining unimplemented Validator methods (the easy ones) - still some remain.

---
+++ trunk/src/Sanitizer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @author    jah <jah@jahboite.co.uk>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+
+
+/**
+ * The Sanitizer interface defines a set of methods for sanitizing untrusted
+ * input to remove all but a whitelist of characters. Implementors should feel
+ * free to extend this interface to accommodate their own data formats.
+ *
+ * PHP version 5.2.9
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @version   1.0
+ * @author    jah <jah@jahboite.co.uk>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+interface Sanitizer
+{    
+    /**
+     * Returns valid, "safe" HTML.
+     * 
+     * This implementation uses HTMLPurifier {@link http://htmlpurifier.org}. 
+     * 
+     * @param  $context A descriptive name of the parameter that you are
+     *         validating (e.g. ProfilePage_Sig). This value is used by any
+     *         logging or error handling that is done with respect to the value
+     *         passed in.
+     * @param  $input The actual user input data to validate.
+     *
+     * @return valid, "safe" HTML.
+     */
+    function getSanitizedHTML($context, $input);
+
+}
---
+++ trunk/src/reference/DefaultSanitizer.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @author    jah <jah@jahboite.co.uk>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+
+
+/**
+ * DefaultSanitizer requires the Sanitizer Interface and the various
+ * ValidationRule implementations.
+ */
+require_once dirname ( __FILE__ ) . '/../Sanitizer.php';
+require_once dirname ( __FILE__ ) . '/validation/StringValidationRule.php';
+require_once dirname ( __FILE__ ) . '/validation/CreditCardValidationRule.php';
+require_once dirname ( __FILE__ ) . '/validation/HTMLValidationRule.php';
+require_once dirname ( __FILE__ ) . '/validation/NumberValidationRule.php';
+require_once dirname ( __FILE__ ) . '/validation/IntegerValidationRule.php';
+require_once dirname ( __FILE__ ) . '/validation/DateValidationRule.php';
+
+
+/**
+ * Reference Implementation of the Sanitizer Interface.
+ *
+ * PHP version 5.2.9
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @version   1.0
+ * @author    jah <jah@jahboite.co.uk>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+class DefaultSanitizer implements Sanitizer
+{
+    
+    private $encoder = null;
+    
+    public function __construct()
+    {
+        global $ESAPI;
+        $this->encoder = ESAPI::getEncoder();
+    }
+	
+	
+	/**
+     * Returns valid, "safe" HTML.
+     * 
+     * This implementation uses HTMLPurifier {@link http://htmlpurifier.org}. 
+     * 
+     * @param  $context A descriptive name of the parameter that you are
+     *         validating (e.g. ProfilePage_Sig). This value is used by any
+     *         logging or error handling that is done with respect to the value
+     *         passed in.
+     * @param  $input The actual user input data to validate.
+     *
+     * @return valid, "safe" HTML.
+     */
+    function getSanitizedHTML($context, $input)
+    {
+        $hvr = new HTMLValidationRule('HTML_Validator', $this->encoder);
+        
+        return $hvr->sanitize($context, $input);
+    }
+    
+
+}
