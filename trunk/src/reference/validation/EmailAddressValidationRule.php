90efe75eff7752d6a3c0b7f41ba717a49e257e37
mike.boberski <mike.boberski@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-03-31 14:02:59 +0000

    Fixed rule name when initializing logger.

--- trunk/src/reference/validation/EmailAddressValidationRule.php
+++ trunk/src/reference/validation/EmailAddressValidationRule.php
@@ -59,7 +59,7 @@ class EmailAddressValidationRule extends StringValidationRule
 
         parent::__construct($typeName, $encoder);
 
-        $this->logger = $ESAPI->getLogger("HTMLValidationRule");
+        $this->logger = $ESAPI->getLogger("EmailAddressValidationRule");
     }
 
     /**
