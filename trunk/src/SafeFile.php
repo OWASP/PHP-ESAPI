ee7d8e23990be17a6ddd9d731491c638a97952ed
mike.boberski <mike.boberski@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-04-08 20:40:57 +0000

    Some PEAR coding standard updates.

--- trunk/src/SafeFile.php
+++ trunk/src/SafeFile.php
@@ -40,94 +40,124 @@ require_once dirname(__FILE__).'/errors/ValidationException.php';
  * @version   Release: @package_version@
  * @link      http://www.owasp.org/index.php/ESAPI
  */
-class SafeFile extends SplFileObject 
+class SafeFile extends SplFileObject
 {
 
-        private $PERCENTS_PAT = "/(%)([0-9a-f])([0-9a-f])/i";
-        private $FILE_BLACKLIST_PAT = "/([\\/:*?<>|])/";
-        private $DIR_BLACKLIST_PAT = "/([*?<>|])/";
+    private $_PERCENTS_PAT = "/(%)([0-9a-f])([0-9a-f])/i";
+    private $_FILE_BLACKLIST_PAT = "/([\\/:*?<>|])/";
+    private $_DIR_BLACKLIST_PAT = "/([*?<>|])/";
 
-        /**
-         * Creates an extended SplFileObject from the given filename, which
-         * prevents against null byte injections and unprintable characters.
-         * @param String $path the path to the file (path && file name)
-         */
-
-        function __construct($path) {
-            try {
-                @parent::__construct($path);
-            } catch (Exception $e) {
-                throw new EnterpriseSecurityException("Failed to open stream", "Failed to open stream " . $e->getMessage());
-            }
+    /**
+     * Creates an extended SplFileObject from the given filename, which
+     * prevents against null byte injections and unprintable characters.
+     * 
+     * @param string $path the path to the file (path && file name)
+     * 
+     * @return does not return a value.
+     */
+    function __construct($path) 
+    {
+        try {
+            @parent::__construct($path);
+        } catch (Exception $e) {
+                throw new EnterpriseSecurityException(
+                    "Failed to open stream", 
+                    "Failed to open stream ".$e->getMessage()
+                );
+        }
             
-            $this->doDirCheck($this->getPath());
-            $this->doFileCheck($this->getFilename());
-            $this->doExtraCheck($path);
+        $this->_doDirCheck($this->getPath());
+        $this->_doFileCheck($this->getFilename());
+        $this->_doExtraCheck($path);
+    }
+
+    /**
+     * Checks the directory against null bytes and unprintable characters.
+     * 
+     * @param string $path the directory path (without the file name)
+     * 
+     * @return does not return a value
+     * @exception ValidationException thrown if check fails
+     */
+    private function _doDirCheck($path) 
+    {
+        if ( preg_match($this->_DIR_BLACKLIST_PAT, $path) ) {
+            throw new ValidationException("Invalid directory", 
+            "Directory path (" + $path + ") contains illegal character. ");
         }
 
-        /**
-         * Checks the directory against null bytes and unprintable characters.
-         * @param String $path the directory path (without the file name)
-         */
-        private function doDirCheck($path) {
-            if ( preg_match($this->DIR_BLACKLIST_PAT, $path) ) {
-                throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains illegal character. ");
-            }
-
-            if ( preg_match($this->PERCENTS_PAT, $path) ) {
-                throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains encoded characters. ");
-            }
-
-            $ch = $this->containsUnprintableCharacters($path);
-            if ($ch != -1) {
-                throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains unprintable character. ");
-            }
+        if ( preg_match($this->_PERCENTS_PAT, $path) ) {
+            throw new ValidationException("Invalid directory", 
+            "Directory path (" + $path + ") contains encoded characters. ");
         }
 
-        /**
-         * Checks the file name against null bytes and unprintable characters.
-         * @param String $path the file name
-         */
-        private function doFileCheck($path) {
-            if ( preg_match($this->FILE_BLACKLIST_PAT, $path) ) {
-                throw new ValidationException("Invalid directory", "Directory path (" + $path + ") contains illegal character.");
-            }
+        $ch = $this->containsUnprintableCharacters($path);
+        if ($ch != -1) {
+            throw new ValidationException("Invalid directory", 
+            "Directory path (" + $path + ") contains unprintable character. ");
+        }
+    }
 
-            if ( preg_match($this->PERCENTS_PAT, $path) ) {
-                throw new ValidationException("Invalid file", "File path (" + $path + ") contains encoded characters.");
-            }
-
-            $ch = $this->containsUnprintableCharacters($path);
-            if ($ch != -1) {
-                throw new ValidationException("Invalid file", "File path (" + $path + ") contains unprintable character.");
-            }
+    /**
+     * Checks the file name against null bytes and unprintable characters.
+     * 
+     * @param string $path the file name
+     * 
+     * @return does not return a value
+     * @exception ValidationException thrown if check fails
+     */
+    private function _doFileCheck($path) 
+    {
+        if ( preg_match($this->_FILE_BLACKLIST_PAT, $path) ) {
+            throw new ValidationException("Invalid directory", 
+            "Directory path (" + $path + ") contains illegal character.");
         }
 
-        /**
-         * Checks the specified string for unprintable characters (ASCII range
-         * from 0 to 31 and from 127 to 255).
-         * @param String $s the String to check for unprintable characters
-         * @return int  the value of the first unprintable character found.
-         *              If no unprintable character was found, -1 is returned.
-         */
-        private function containsUnprintableCharacters($s) {
-            for ($i = 0; $i < strlen($s); $i++) {
-                $ch = $s[$i];
-                if (ord($ch) < 32 || ord($ch) > 126) {
-                    return $ch;
-                }
-            }
-            return -1;
+        if ( preg_match($this->_PERCENTS_PAT, $path) ) {
+            throw new ValidationException("Invalid file", 
+            "File path (" + $path + ") contains encoded characters.");
         }
+
+        $ch = $this->containsUnprintableCharacters($path);
+        if ($ch != -1) {
+            throw new ValidationException("Invalid file", 
+            "File path (" + $path + ") contains unprintable character.");
+        }
+    }
+
+    /**
+     * Checks the specified string for unprintable characters (ASCII range
+     * from 0 to 31 and from 127 to 255).
+     * 
+     * @param string $s the string to check for unprintable characters
+     * 
+     * @return int the value of the first unprintable character found, or -1
+     */
+    private function _containsUnprintableCharacters($s) 
+    {
+        for ($i = 0; $i < strlen($s); $i++) {
+            $ch = $s[$i];
+            if (ord($ch) < 32 || ord($ch) > 126) {
+                return $ch;
+            }
+        }
+        return -1;
+    }
         
-        /*
-         * Checks if the last character is a slash
-         * @param String $path the string to check
-         */
-        private function doExtraCheck($path) {
-        	$last = substr($path, -1);
-        	if ($last === '/') {
-        		throw new ValidationException("Invalid file", "File path (" + $path + ") contains an extra slash.");
-        	}
+    /**
+     * Checks if the last character is a slash
+     * 
+     * @param string $path the string to check
+     * 
+     * @return does not return a value
+     * @exception ValidationException thrown if check fails
+     */
+    private function _doExtraCheck($path) 
+    {
+        $last = substr($path, -1);
+        if ($last === '/') {
+            throw new ValidationException("Invalid file", 
+            "File path (" + $path + ") contains an extra slash.");
         }
+    }
 }
