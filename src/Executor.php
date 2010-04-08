3ce61bea19c798d5ceafc25685764b2cd53b97f1
mike.boberski <boberski_michael@bah.com>
2010-04-08 18:19:00 +0000

    Some PEAR coding standard updates.

--- src/Executor.php
+++ src/Executor.php
@@ -15,6 +15,7 @@
  * @package   ESAPI
  * @author    Jeff Williams <jeff.williams@aspectsecurity.com>
  * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
  * @copyright 2009-2010 The OWASP Foundation
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
  * @version   SVN: $Id$
@@ -47,6 +48,7 @@ require_once dirname(__FILE__) . '/errors/ExecutorException.php';
  * @author    Andrew van der Stock <vanderaj@owasp.org>
  * @author    Mike Boberski <boberski_michael@bah.com>
  * @author    Linden Darling <linden.darling@jds.net.au>
+ * @author    Mike Boberski <boberski_michael@bah.com>
  * @copyright 2009-2010 The OWASP Foundation
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
  * @version   Release: @package_version@
@@ -56,41 +58,39 @@ interface Executor
 {
 
     /**
-         * Invokes the specified executable with default workdir and not logging parameters.
-         * 
-         * @param executable
-         *            the command to execute
-         * @param params
-         *            the parameters of the command being executed
-         */
-        function executeSystemCommand($executable, $params);
+     * Invokes the specified executable with default workdir and not logging 
+     * parameters.
+     * 
+     * @param string $executable the command to execute
+     * @param array  $params     the parameters of the command being executed
+     * 
+     * @return does not return a value.
+     */
+    function executeSystemCommand($executable, $params);
 
     /**
-         * Executes a system command after checking that the executable exists and
-         * escaping all the parameters to ensure that injection is impossible.
-         * Implementations must change to the specified working
-         * directory before invoking the command.
-         * 
-         * @param executable
-         *            the command to execute
-         * @param params
-         *            the parameters of the command being executed
-         * @param workdir
-         *            the working directory
-         * @param codec
-         *            the codec to use to encode for the particular OS in use
-         * @param logParams
-         *            use false if any parameters contains sensitive or confidential information.
-         *            (this is an ESAPI 2.0 feature)
-         * 
-         * @return the output of the command being run
-         * 
-         * @throws ExecutorException
-         *             the service exception
-         *             
-         * note: this is PHP's equivalent to ESAPI4JAVA's overloaded executeSystemCommand($executable, $params, $workdir, $codec, $logParams)
-         * note: the codec argument has been eliminated from this implementation since PHP's escapeshellcmd function does enough to not require explicit OS codecs
-         */
-        function executeSystemCommand_effectLonghand($executable, $params, $workdir, $logParams);
+     * Executes a system command after checking that the executable exists and
+     * escaping all the parameters to ensure that injection is impossible.
+     * Implementations must change to the specified working
+     * directory before invoking the command.
+     *             
+     * note: this is PHP's equivalent to ESAPI4JAVA's overloaded 
+     * executeSystemCommand($executable, $params, $workdir, $codec, $logParams)
+     * note: the codec argument has been eliminated from this implementation since 
+     * PHP's escapeshellcmd function does enough to not require explicit OS codecs
+     * 
+     * @param string $executable the command to execute
+     * @param array  $params     the parameters of the command being executed
+     * @param string $workdir    the working directory
+     * @param bool   $logParams  use false if any parameters contains sensitive or 
+     *                           confidential information. (this is an ESAPI 2.0 
+     *                           feature)
+     * 
+     * @return string the output of the command being run
+     * @throws ExecutorException the service exception
+     */
+     function executeSystemCommandLonghand($executable, $params, $workdir, 
+         $logParams
+     );
 
 }
