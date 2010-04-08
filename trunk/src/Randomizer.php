7b7c374f147d1257eaf47345bfdc71521ec05869
mike.boberski <mike.boberski@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-04-08 20:08:32 +0000

    Some PEAR coding standard updates.

--- trunk/src/Randomizer.php
+++ trunk/src/Randomizer.php
@@ -43,110 +43,111 @@ require_once dirname(__FILE__) . '/errors/EncryptionException.php';
  * @version   Release: @package_version@
  * @link      http://www.owasp.org/index.php/ESAPI
  */
-interface Randomizer 
+interface Randomizer
 {
 
-	/**
-	 * Gets a random string of a desired length and character set.  The use of java.security.SecureRandom
-	 * is recommended because it provides a cryptographically strong pseudo-random number generator. 
-	 * If SecureRandom is not used, the pseudo-random number gernerator used should comply with the 
-	 * statistical random number generator tests specified in <a href="http://csrc.nist.gov/cryptval/140-2.htm">
-	 * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, section 4.9.1.
-	 * 
-	 * @param length 
-	 * 		the length of the string
-	 * @param characterSet 
-	 * 		the set of characters to include in the created random string
-	 * 
-	 * @return 
-	 * 		the random string of the desired length and character set
-	 */
-	function getRandomString($length, $characterSet);
-
-	/**
-	 * Returns a random boolean.  The use of java.security.SecureRandom
-	 * is recommended because it provides a cryptographically strong pseudo-random number generator. 
-	 * If SecureRandom is not used, the pseudo-random number gernerator used should comply with the 
-	 * statistical random number generator tests specified in <a href="http://csrc.nist.gov/cryptval/140-2.htm">
-	 * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, section 4.9.1.
-	 * 
-	 * @return 
-	 * 		true or false, randomly
-	 */
-	function getRandomBoolean();
-	
-	/**
-	 * Gets the random integer. The use of java.security.SecureRandom
-	 * is recommended because it provides a cryptographically strong pseudo-random number generator. 
-	 * If SecureRandom is not used, the pseudo-random number gernerator used should comply with the 
-	 * statistical random number generator tests specified in <a href="http://csrc.nist.gov/cryptval/140-2.htm">
-	 * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, section 4.9.1.
-	 * 
-	 * @param min 
-	 * 		the minimum integer that will be returned
-	 * @param max 
-	 * 		the maximum integer that will be returned
-	 * 
-	 * @return 
-	 * 		the random integer
-	 */
-	function getRandomInteger($min, $max);
-
-	
-	/**
-	 * Gets the random long. The use of java.security.SecureRandom
-	 * is recommended because it provides a cryptographically strong pseudo-random number generator. 
-	 * If SecureRandom is not used, the pseudo-random number gernerator used should comply with the 
-	 * statistical random number generator tests specified in <a href="http://csrc.nist.gov/cryptval/140-2.htm">
-	 * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, section 4.9.1.
-	 * 
-	 * @return 
-	 * 		the random long
-	 */
-    function getRandomLong();
-	
-	
     /**
-     * Returns an unguessable random filename with the specified extension.  This method could call
-     * getRandomString(length, charset) from this Class with the desired length and alphanumerics as the charset 
+     * Gets a random string of a desired length and character set.  The use of 
+     * java.security.SecureRandom is recommended because it provides a 
+     * cryptographically strong pseudo-random number generator. If SecureRandom 
+     * is not used, the pseudo-random number gernerator used should comply with the 
+     * statistical random number generator tests specified in 
+     * <a href="http://csrc.nist.gov/cryptval/140-2.htm">
+     * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, section 
+     * 4.9.1.
+     * 
+     * @param int    $length       the length of the string
+     * @param string $characterSet the set of characters to include in the created 
+     *                             random string
+     * 
+     * @return string the random string of the desired length and character set
+     */
+    function getRandomString($length, $characterSet);
+
+    /**
+     * Returns a random boolean.  The use of java.security.SecureRandom
+     * is recommended because it provides a cryptographically strong 
+     * pseudo-random number generator. If SecureRandom is not used, the 
+     * pseudo-random number gernerator used should comply with the 
+     * statistical random number generator tests specified in 
+     * <a href="http://csrc.nist.gov/cryptval/140-2.htm">
+     * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, 
+     * section 4.9.1.
+     * 
+     * @return bool true or false, randomly
+     */
+    function getRandomBoolean();
+
+    /**
+     * Gets the random integer. The use of java.security.SecureRandom
+     * is recommended because it provides a cryptographically strong 
+     * pseudo-random number generator. If SecureRandom is not used, the
+     *  pseudo-random number gernerator used should comply with the 
+     * statistical random number generator tests specified in 
+     * <a href="http://csrc.nist.gov/cryptval/140-2.htm">
+     * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, 
+     * section 4.9.1.
+     * 
+     * @param int $min the minimum integer that will be returned
+     * @param int $max the maximum integer that will be returned
+     * 
+     * @return int the random integer
+     */
+    function getRandomInteger($min, $max);
+
+
+    /**
+     * Gets the random long. The use of java.security.SecureRandom
+     * is recommended because it provides a cryptographically strong 
+     * pseudo-random number generator. If SecureRandom is not used, the 
+     * pseudo-random number gernerator used should comply with the 
+     * statistical random number generator tests specified in 
+     * <a href="http://csrc.nist.gov/cryptval/140-2.htm">
+     * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, 
+     * section 4.9.1.
+     * 
+     * @return int the random long
+     */
+    function getRandomLong();
+
+
+    /**
+     * Returns an unguessable random filename with the specified extension.  
+     * This method could call getRandomString(length, charset) from this 
+     * Class with the desired length and alphanumerics as the charset 
      * then merely append "." + extension.
      * 
-     * @param extension 
-     * 		extension to add to the random filename
+     * @param string $extension extension to add to the random filename
      * 
-     * @return 
-     * 		a random unguessable filename ending with the specified extension
+     * @return string a random unguessable filename ending with the specified 
+     *                extension
      */
     function getRandomFilename( $extension = '' );
     
     
-	/**
-	 * Gets the random real.  The use of java.security.SecureRandom
-	 * is recommended because it provides a cryptographically strong pseudo-random number generator. 
-	 * If SecureRandom is not used, the pseudo-random number gernerator used should comply with the 
-	 * statistical random number generator tests specified in <a href="http://csrc.nist.gov/cryptval/140-2.htm">
-	 * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, section 4.9.1.
-	 * 
-	 * @param min 
-	 * 		the minimum real number that will be returned
-	 * @param max 
-	 * 		the maximum real number that will be returned
-	 * 
-	 * @return 
-	 * 		the random real
-	 */
-	function getRandomReal($min, $max);
+    /**
+     * Gets the random real.  The use of java.security.SecureRandom
+     * is recommended because it provides a cryptographically strong pseudo-random 
+     * number generator. If SecureRandom is not used, the pseudo-random number 
+     * generator used should comply with the statistical random number generator 
+     * tests specified in <a href="http://csrc.nist.gov/cryptval/140-2.htm">
+     * FIPS 140-2, Security Requirements for Cryptographic Modules</a>, section 
+     * 4.9.1.
+     * 
+     * @param float $min the minimum real number that will be returned
+     * @param float $max the maximum real number that will be returned
+     * 
+     * @return float the random real
+     */
+    function getRandomReal($min, $max);
 
     /**
-     * Generates a random GUID.  This method could use a hash of random Strings, the current time,
-     * and any other random data available.  The format is a well-defined sequence of 32 hex digits 
-     * grouped into chunks of 8-4-4-4-12.  
+     * Generates a random GUID.  This method could use a hash of random Strings, 
+     * the current time, and any other random data available.  The format is a 
+     * well-defined sequence of 32 hex digits grouped into chunks of 8-4-4-4-12.  
      * 
-     * @return 
-     * 		the GUID
-     * 
-     * @throws 
-     * 		EncryptionException if hashing or encryption fails 
+     * @return string the GUID
+     * @throws EncryptionException if hashing or encryption fails 
      */
     function getRandomGUID();
            
