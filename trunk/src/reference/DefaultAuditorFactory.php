004904363c877cdbcd97f8ac582c56e0e7b34bda
mike.boberski <mike.boberski@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-04-14 19:50:57 +0000

    Some PEAR coding standard updates.

--- trunk/src/reference/DefaultAuditorFactory.php
+++ trunk/src/reference/DefaultAuditorFactory.php
@@ -3,18 +3,24 @@
  * OWASP Enterprise Security API (ESAPI)
  *
  * This file is part of the Open Web Application Security Project (OWASP)
- * Enterprise Security API (ESAPI) project. For details, please see
- * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ * Enterprise Security API (ESAPI) project.
+ * 
+ * PHP version 5.2
  *
- * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
  *
- * The ESAPI is published by OWASP under the BSD license. You should read and
- * accept the LICENSE before you use, modify, and/or redistribute this software.
- *
- * @author Jeff Williams <a href="http://www.aspectsecurity.com">Aspect Security</a>
- * @since  2008
- * @since  1.6
- * @package org-owasp-esapi-reference
+ * @category  OWASP
+ * @package   ESAPI
+ * @author    Jeff Williams <jeff.williams@aspectsecurity.com>
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Laura Bell <laura.d.bell@gmail.com>
+ * @author    jah <jah@jahboite.co.uk>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   SVN: $Id$
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 
 
@@ -26,23 +32,29 @@ require_once dirname(__FILE__).'/DefaultAuditor.php';
 
 
 /**
- * Reference implementation of the ESAPILogFactory interface.
+ * Reference Implementation of the DefaultAuditorFactory interface.
  *
- * This implementation maintains an associative array of DefaultLogger instances
- * which may retrieved by name.  Instances of DefaultLogger are created only if
- * the name does not exist as a key in the array.
- *
- * @author Laura D. Bell
- * @since  1.6
+ * @category  OWASP
+ * @package   ESAPI
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Laura Bell <laura.d.bell@gmail.com>
+ * @author    jah <jah@jahboite.co.uk>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   Release: @package_version@
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
-class DefaultAuditorFactory implements AuditorFactory {
+class DefaultAuditorFactory implements AuditorFactory
+{
 
-    private $loggerMap = array();
+    private $_loggerMap = array();
 
 
     /**
-     * Null argument constructor for this implementation of the LogFactory
-     * interface needed for dynamic configuration.
+     * DefaultAuditorFactory constructor.
+     * 
+     * @return does not return a value.
      */
     function __construct()
     {
@@ -51,19 +63,20 @@ class DefaultAuditorFactory implements AuditorFactory {
 
 
     /**
-     * {@inheritDoc}
+     * @inheritDoc
      */
-    public function getLogger($moduleName) {
+    public function getLogger($moduleName) 
+    {
 
         // If a logger for this module already exists, we return the same one,
         // otherwise we create a new one.
-        if (   array_key_exists($moduleName, $this->loggerMap)
-            && $this->loggerMap[$moduleName] instanceof DefaultAuditor
+        if (   array_key_exists($moduleName, $this->_loggerMap)
+            && $this->_loggerMap[$moduleName] instanceof DefaultAuditor
         ) {
-            return $this->loggerMap[$moduleName];
+            return $this->_loggerMap[$moduleName];
         } else {
             $moduleLogger = new DefaultAuditor($moduleName);
-            $this->loggerMap[$moduleName] = $moduleLogger;
+            $this->_loggerMap[$moduleName] = $moduleLogger;
             return $moduleLogger;
         }
     }
