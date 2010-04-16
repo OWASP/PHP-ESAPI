50fefaa41e9c700c2ac360d3d754ae6d39422a08
mike.boberski <mike.boberski@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-04-16 14:16:42 +0000

    Some PEAR coding standard updates.

--- trunk/src/reference/DefaultExecutor.php
+++ trunk/src/reference/DefaultExecutor.php
@@ -1,154 +1,158 @@
 <?php
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
- * This file is part of the Open Web Application Security Project (OWASP)
- * Enterprise Security API (ESAPI) project. For details, please see
- * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
  * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
- * 
+ * PHP version 5.2
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
+ *
  * @category  OWASP
  * @package   ESAPI
- * @version   1.0 
  * @author    Mike Boberski <boberski_michael@bah.com> 
  * @author    Linden Darling <linden.darling@jds.net.au>
- *  
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   SVN: $Id$
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 
 require_once  dirname(__FILE__).'/../Executor.php';
 
-class DefaultExecutor implements Executor {
-		
-	// Logger
-	private $logger = null;
-	private $ApplicationName = null;
-	private $LogEncodingRequired = null;
-	private $LogLevel = null;
-	private $LogFileName = null;
-	private $MaxLogFileSize = null;
-	
-	//SecurityConfiguration
-	private $config = null;
-	
-	function __construct()
-	{
-		$this->logger = ESAPI::getAuditor('Executor');
-		$this->config = ESAPI::getSecurityConfiguration();
- 	}
+/**
+ * Reference Implementation of the Executor interface.
+ *
+ * @category  OWASP
+ * @package   ESAPI
+ * @author    Mike Boberski <boberski_michael@bah.com> 
+ * @author    Linden Darling <linden.darling@jds.net.au>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   Release: @package_version@
+ * @link      http://www.owasp.org/index.php/ESAPI
+ */
+class DefaultExecutor implements Executor
+{
+        
+    // Logger
+    private $_auditor = null;
+    private $_ApplicationName = null;
+    private $_LogEncodingRequired = null;
+    private $_LogLevel = null;
+    private $_LogFileName = null;
+    private $_MaxLogFileSize = null;
+    
+    //SecurityConfiguration
+    private $_config = null;
+    
+    /**
+     * Executor constructor.
+     * 
+     * @return does not return a value.
+     */
+    function __construct()
+    {
+        $this->_auditor = ESAPI::getAuditor('Executor');
+        $this->_config = ESAPI::getSecurityConfiguration();
+    }
 
     /**
-         * Invokes the specified executable with default workdir and not logging parameters.
-         * 
-         * @param executable
-         *            the command to execute
-         * @param params
-         *            the parameters of the command being executed
-         */
-	function executeSystemCommand($executable, $params)
-	{
-		$workdir = $this->config->getWorkingDirectory();
-    	$logParams = false;
-    	return $this->executeSystemCommandLonghand($executable, $params, $workdir, $logParams);
-	}
- 	
- 	/**
-         * Executes a system command after checking that the executable exists and
-         * escaping all the parameters to ensure that injection is impossible.
-         * Implementations must change to the specified working
-         * directory before invoking the command.
-         * 
-         * @param executable
-         *            the command to execute
-         * @param params
-         *            the parameters of the command being executed
-         * @param workdir
-         *            the working directory
-         * @param codec
-         *            the codec to use to encode for the particular OS in use
-         * @param logParams
-         *            use false if any parameters contains sensitive or confidential information.
-         *            (this is an ESAPI 2.0 feature)
-         * 
-         * @return the output of the command being run
-         * 
-         * @throws ExecutorException
-         *             the service exception
-         *             
-         * note: this is PHP's equivalent to ESAPI4JAVA's overloaded executeSystemCommand($executable, $params, $workdir, $codec, $logParams)
-         * note: the codec argument has been eliminated from this implementation since PHP's escapeshellcmd function does enough to not require explicit OS codecs
-         */
-	function executeSystemCommandLonghand($executable, $params, $workdir, $logParams)
-	{
-    	try {
-    		
-	        // executable must exist
-	        $resolved = $executable;
-			if(substr(PHP_OS, 0, 3) == 'WIN')
-			{
-	        	$exploded = explode("%", $executable);
-	        	$systemroot = getenv($exploded[1]);
-	        	$resolved = $systemroot . $exploded[2];
-			}
-            if (!file_exists($resolved))
-            {
-                throw new ExecutorException("Execution failure, No such executable: $executable");
+     * @inheritdoc
+     */
+    function executeSystemCommand($executable, $params)
+    {
+        $workdir = $this->_config->getWorkingDirectory();
+        $logParams = false;
+        return $this->executeSystemCommandLonghand(
+            $executable, $params, $workdir, $logParams
+        );
+    }
+     
+     /**
+     * @inheritdoc
+     */
+    function executeSystemCommandLonghand($executable, $params, $workdir, 
+        $logParams
+    ) {
+        try {
+            
+            // executable must exist
+            $resolved = $executable;
+            
+            if (substr(PHP_OS, 0, 3) == 'WIN') {
+                $exploded = explode("%", $executable);
+                $systemroot = getenv($exploded[1]);
+                $resolved = $systemroot . $exploded[2];
+            }
+            
+            if (!file_exists($resolved)) {
+                throw new ExecutorException(
+                    "Execution failure, No such ".
+                    "executable: $executable"
+                );
             }
             
             // executable must use canonical path
-            if (!strcmp($resolved, realpath($resolved)))
-            {
-                throw new ExecutorException("Execution failure, Attempt to invoke an executable using a non-absolute path: $executable");
+            if (!strcmp($resolved, realpath($resolved))) {
+                throw new ExecutorException(
+                    "Execution failure, Attempt ".
+                    "to invoke an executable using a non-absolute path: $executable"
+                );
             }            
-                     		
-            // exact, absolute, canonical path to executable must be listed in ESAPI configuration 
-            $approved = $this->config->getAllowedExecutables();
-            if(!in_array($executable, $approved))
-            {
-                throw new ExecutorException("Execution failure, Attempt to invoke executable that is not listed as an approved executable in ESAPI configuration: " . $executable . " not listed in " . $approved);
+                             
+            // exact, absolute, canonical path to executable must be listed 
+            //in ESAPI configuration 
+            $approved = $this->_config->getAllowedExecutables();
+            if (!in_array($executable, $approved)) {
+                throw new ExecutorException(
+                    "Execution failure, Attempt to invoke executable that ".
+                    "is not listed as an approved executable in ESAPI ".
+                    "configuration: ".$executable . " not listed in " . $approved
+                );
             }            
 
             // escape any special characters in the parameters
-            for ($i = 0; $i < count($params); $i++)
-            {
-            	$params[$i] = escapeshellcmd($params[$i]);  
+            for ($i = 0; $i < count($params); $i++) {
+                $params[$i] = escapeshellcmd($params[$i]);  
             }
            
             // working directory must exist
-	        $resolved_workdir = $workdir;
-			if(substr(PHP_OS, 0, 3) == 'WIN')
-			{
-				if(substr_count($workdir,'%')>=2)
-				{
-					//only explode on % if at least 2x % chars exist in string
-		        	$exploded = explode("%", $workdir);
-		        	$systemroot = getenv($exploded[1]);
-		        	$resolved_workdir = $systemroot . $exploded[2];
-				}
-			}
-            if(!file_exists($resolved_workdir))
-            {
-                throw new ExecutorException("Execution failure, No such working directory for running executable: $workdir");
+            $resolved_workdir = $workdir;
+            if (substr(PHP_OS, 0, 3) == 'WIN') {
+                if (substr_count($workdir, '%')>=2) {
+                    //only explode on % if at least 2x % chars exist in string
+                    $exploded = explode("%", $workdir);
+                    $systemroot = getenv($exploded[1]);
+                    $resolved_workdir = $systemroot . $exploded[2];
+                }
+            }
+            if (!file_exists($resolved_workdir)) {
+                throw new ExecutorException(
+                    "Execution failure, No such".
+                    " working directory for running executable: $workdir"
+                );
             }
  
             // run the command
             $paramstr = "";
-            foreach($params as $param)
-            {
-            	$paramstr .= " ".$param;	//note: will yeild a paramstr with a leading whitespace
+            foreach ($params as $param) {
+                //note: will yeild a paramstr with a leading whitespace
+                $paramstr .= " ".$param;    
             }
-            $output = $executable.$paramstr;	//note: no whitespace between $executable and $paramstr since $paramstr already has a leading whitespace
+            //note: no whitespace between $executable and $paramstr since 
+            //$paramstr already has a leading whitespace
+            $output = $executable.$paramstr;    
             return $output;
-    	}
-    	catch ( Exception $e )
-    	{
-        	$this->logger->warning(Auditor::SECURITY, true, $e->getMessage());
         }
-	
-	}	
-	
+        catch ( Exception $e ) {
+            $this->_auditor->warning(Auditor::SECURITY, true, $e->getMessage());
+        }
+    
+    }    
+    
 }
 ?>
