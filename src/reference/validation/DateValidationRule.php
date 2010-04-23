d61cc4b2245cc8a84ccccc41e47dabcc9d0889cd
mike.boberski <boberski_michael@bah.com>
2010-04-23 18:00:54 +0000

    Some PEAR coding standard updates.

--- src/reference/validation/DateValidationRule.php
+++ src/reference/validation/DateValidationRule.php
@@ -84,7 +84,8 @@ class DateValidationRule extends BaseValidationRule
     {
         if (! is_string($newFormat) || $newFormat == '') {
             throw new RuntimeException(
-                'setDateFormat requires a non-empty string DateFormat as accepted by date().'
+                'setDateFormat requires a non-empty string DateFormat as '.
+                'accepted by date().'
             );
         }
         $this->_format = $newFormat;
@@ -141,7 +142,8 @@ class DateValidationRule extends BaseValidationRule
         {
             throw new ValidationException(
                 "{$context} -  Invalid input. Encoding problem detected.",
-                'An EncodingException was thrown during canonicalization of the input.',
+                'An EncodingException was thrown during canonicalization of '.
+                'the input.',
                 $context
             );
         }
