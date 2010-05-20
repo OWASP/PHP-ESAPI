78a5dedcc819ded8d63bc6d4a11c35803984b270
mike.boberski <boberski_michael@bah.com>
2010-05-20 20:15:11 +0000

    Some PEAR coding standard updates.

--- src/errors/EncodingException.php
+++ src/errors/EncodingException.php
@@ -1,20 +1,24 @@
 <?php
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
- * This file is part of the Open Web Application Security Project (OWASP)
- * Enterprise Security API (ESAPI) project. For details, please see
- * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
  * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
- * 
- * @author Andrew van der Stock <vanderaj .(at). owasp.org> 
- * @created 2008
- * @since 1.4
- * @package ESAPI_Errors
+ * PHP version 5.2
+ *
+ * @category  OWASP
+ * @package   ESAPI_Errors
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   SVN: $Id$
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 
 require_once  dirname(__FILE__).'/EnterpriseSecurityException.php';
@@ -22,20 +26,25 @@ require_once  dirname(__FILE__).'/EnterpriseSecurityException.php';
 /**
  * An EncodingException should be thrown for any problems that occur when
  * encoding or decoding data.
- * 
- * @author Jeff Williams (jeff.williams@aspectsecurity.com)
+ *
+ * @category  OWASP
+ * @package   ESAPI_Errors
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   Release: @package_version@
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 class EncodingException extends EnterpriseSecurityException
 {
     /**
      * Instantiates a new EncodingException.
      * 
-     * @param userMessage
-     *            the message displayed to the user
-     * @param logMessage
-     * 			  the message logged
-     * @param cause
-     *            the cause
+     * @param string $userMessage the message displayed to the user
+     * @param string $logMessage  the message logged
+     * 
+     * @return does not return a value.
      */
     function __construct($userMessage = '', $logMessage = '')
     {
--- src/errors/ValidationException.php
+++ src/errors/ValidationException.php
@@ -1,20 +1,24 @@
 <?php
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
- * This file is part of the Open Web Application Security Project (OWASP)
- * Enterprise Security API (ESAPI) project. For details, please see
- * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
  * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
- * 
- * @author Andrew van der Stock <vanderaj .(at). owasp.org> 
- * @created 2008
- * @since 1.4
- * @package ESAPI_Errors
+ * PHP version 5.2
+ *
+ * @category  OWASP
+ * @package   ESAPI_Errors
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   SVN: $Id$
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 
 require_once  dirname(__FILE__).'/EnterpriseSecurityException.php';
@@ -23,24 +27,31 @@ require_once  dirname(__FILE__).'/EnterpriseSecurityException.php';
  * A ValidationException should be thrown to indicate that the data provided by
  * the user or from some other external source does not match the validation
  * rules that have been specified for that data.
+ *
+ * @category  OWASP
+ * @package   ESAPI_Errors
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   Release: @package_version@
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 class ValidationException extends EnterpriseSecurityException
 {
 
     /** The UI reference that caused this ValidationException */
-    private $context;
+    private $_context;
 
     /**
      * Instantiates a new ValidationException.
+     * Create a new ValidationAvailabilityException
      * 
-     * @param userMessage
-     *            the message to display to users
-     * @param logMessage
-     * 			  the message logged
-     * @param cause
-     *            the cause
-     * @param context
-     *            the source that caused this exception
+     * @param string $userMessage the message displayed to the user
+     * @param string $logMessage  the message logged
+     * @param string $context     the source that caused this exception
+     * 
+     * @return does not return a value.
      */
     function __construct($userMessage = '', $logMessage = '', $context = '')
     {
@@ -51,22 +62,24 @@ class ValidationException extends EnterpriseSecurityException
     /**
      * Returns the UI reference that caused this ValidationException
      *  
-     * @return context, the source that caused the exception, stored as a string
+     * @return string context, the source that caused the exception, stored as a 
+     *                string
      */
     public function getContext()
     {
-        return $this->context;
+        return $this->_context;
     }
 
     /**
      * Set's the UI reference that caused this ValidationException
      *  
-     * @param context
-     * 			the context to set, passed as a String
+     * @param string $context the context to set, passed as a String
+     * 
+     * @return does not return a value.
      */
     public function setContext($context)
     {
-        $this->context = $context;
+        $this->_context = $context;
     }
 }
 ?>
