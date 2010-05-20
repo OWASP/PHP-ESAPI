1fa81c4b8d025b6643fc1a52ed3b47fe9cf084af
mike.boberski <mike.boberski@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-05-20 20:15:11 +0000

    Some PEAR coding standard updates.

--- trunk/src/errors/AccessControlException.php
+++ trunk/src/errors/AccessControlException.php
@@ -1,36 +1,50 @@
 <?php
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
- * This file is part of the Open Web Application Security Project (OWASP)
- * Enterprise Security API (ESAPI) project. For details, please see
- * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
  * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
- * 
- * @author Andrew van der Stock <vanderaj .(at). owasp.org> 
- * @created 2008
- * @since 1.4
- * @package ESAPI_Errors
+ * PHP version 5.2
+ *
+ * @category  OWASP
+ * @package   ESAPI_Errors
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   SVN: $Id$
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 
-require_once  dirname(__FILE__).'/EnterpriseSecurityException.php';
+require_once dirname(__FILE__).'/EnterpriseSecurityException.php';
 
 /**
  * An AccessControlException should be thrown when a user attempts to access a
  * resource that they are not authorized for.
+ *
+ * @category  OWASP
+ * @package   ESAPI_Errors
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   Release: @package_version@
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 class AccessControlException extends EnterpriseSecurityException
 {
     /**
      * Instantiates a new access control exception.
      * 
-     * @param userMessage the message displayed to the user
-     * @param logMessage the message logged
-     * @param cause the cause
+     * @param string $userMessage the message displayed to the user
+     * @param string $logMessage  the message logged
+     * 
+     * @return does not return a value.
      */
     function __construct($userMessage = '', $logMessage = '')
     {
--- trunk/src/errors/IntrusionException.php
+++ trunk/src/errors/IntrusionException.php
@@ -1,32 +1,47 @@
 <?php
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
- * This file is part of the Open Web Application Security Project (OWASP)
- * Enterprise Security API (ESAPI) project. For details, please see
- * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
  * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
- * 
- * @author Andrew van der Stock <vanderaj .(at). owasp.org> 
- * @created 2008
- * @since 1.4
- * @package ESAPI_Errors
+ * PHP version 5.2
+ *
+ * @category  OWASP
+ * @package   ESAPI_Errors
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   SVN: $Id$
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 
 require_once  dirname(__FILE__).'/EnterpriseSecurityException.php';
 
 /**
- * An IntrusionException should be thrown anytime an error condition arises that is likely to be the result of an attack
- * in progress. IntrusionExceptions are handled specially by the IntrusionDetector, which is equipped to respond by
- * either specially logging the event, logging out the current user, or invalidating the current user's account.
+ * An IntrusionException should be thrown anytime an error condition arises that 
+ * is likely to be the result of an attack in progress. IntrusionExceptions are 
+ * handled specially by the IntrusionDetector, which is equipped to respond by
+ * either specially logging the event, logging out the current user, or invalidating
+ * the current user's account.
  * <P>
- * Unlike other exceptions in the ESAPI, the IntrusionException is a RuntimeException so that it can be thrown from
- * anywhere and will not require a lot of special exception handling.
- * 
+ * Unlike other exceptions in the ESAPI, the IntrusionException is a 
+ * RuntimeException so that it can be thrown from anywhere and will not require a 
+ * lot of special exception handling.
+ *
+ * @category  OWASP
+ * @package   ESAPI_Errors
+ * @author    Andrew van der Stock <vanderaj@owasp.org>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   Release: @package_version@
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 class IntrusionException extends Exception
 {
@@ -36,13 +51,11 @@ class IntrusionException extends Exception
     /**
      * Instantiates a new intrusion exception.
      * 
-     * @param userMessage
-     *            the message to display to users
-     * @param logMessage
-     * 			  the message logged
-     * @param cause 
-     *			  the cause
-     */
+     * @param string $userMessage the message displayed to the user
+     * @param string $logMessage  the message logged
+     * 
+     * @return does not return a value.
+    */
     function __construct($userMessage = '', $logMessage = '')
     {
         global $ESAPI;
@@ -50,13 +63,15 @@ class IntrusionException extends Exception
         parent::__construct($userMessage);
         $this->logMessage = $logMessage;
         $logger = $ESAPI->getAuditor("IntrusionException");
-        $logger->error(DefaultAuditor::SECURITY, false, "INTRUSION - " . $logMessage);
+        $logger->error(
+            DefaultAuditor::SECURITY, false, "INTRUSION - " . $logMessage
+        );
     }
 
     /**
      * Returns a String containing a message that is safe to display to users
      * 
-     * @return a String containing a message that is safe to display to users
+     * @return string a String containing a message that is safe to display to users
      */
     public function getUserMessage()
     {
@@ -66,7 +81,8 @@ class IntrusionException extends Exception
     /**
      * Returns a String that is safe to display in logs, but probably not to users
      * 
-     * @return a String containing a message that is safe to display in logs, but probably not to users
+     * @return string a String containing a message that is safe to display in 
+     *                logs, but probably not to users
      */
     public function getLogMessage()
     {
