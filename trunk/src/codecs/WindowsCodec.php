cd5f301067d3fdf6aae7e6f28e7efa2c08e4c665
mike.boberski <mike.boberski@9ee01bff-e348-0410-92f4-dfe08fd4fe9e>
2010-05-20 19:29:07 +0000

    Some PEAR coding standard updates.

--- trunk/src/codecs/WindowsCodec.php
+++ trunk/src/codecs/WindowsCodec.php
@@ -1,120 +1,138 @@
 <?php
 /**
  * OWASP Enterprise Security API (ESAPI)
- * 
- * This file is part of the Open Web Application Security Project (OWASP)
- * Enterprise Security API (ESAPI) project. For details, please see
- * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
  *
- * Copyright (c) 2007 - 2009 The OWASP Foundation
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project.
+ *
+ * LICENSE: This source file is subject to the New BSD license.  You should read
+ * and accept the LICENSE before you use, modify, and/or redistribute this
+ * software.
  * 
- * The ESAPI is published by OWASP under the BSD license. You should read and accept the
- * LICENSE before you use, modify, and/or redistribute this software.
- * 
- * @author Linden Darling <a href="http://www.jds.net.au">JDS Australia</a>
- * @author Mike Boberski
- * @created 2009
- * @since 1.6
- * @package ESAPI_Codecs
+ * PHP version 5.2
+ *
+ * @category  OWASP
+ * @package   ESAPI_Codecs
+ * @author    Linden Darling <Linden.Darling@jds.net.au>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   SVN: $Id$
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
 
-
-require_once ('Codec.php');
+require_once 'Codec.php';
 
 /**
- * Implementation of the Codec interface for '^' encoding from Windows command shell.
- * 
- * @author 
- * @since 1.4
- * @see org.owasp.esapi.Encoder
+ * Reference implementation of the Windows codec.
+ *
+ * @category  OWASP
+ * @package   ESAPI_Codecs
+ * @author    Linden Darling <Linden.Darling@jds.net.au>
+ * @author    Mike Boberski <boberski_michael@bah.com>
+ * @copyright 2009-2010 The OWASP Foundation
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
+ * @version   Release: @package_version@
+ * @link      http://www.owasp.org/index.php/ESAPI
  */
- 
 class WindowsCodec extends Codec
 {
-
     /**
      * Public Constructor 
      */
     function __construct()
     {
-  		parent::__construct();
+        parent::__construct();
     }
-
- 
+    
+    
     /**
      * {@inheritDoc}
      */
-    public function encodeCharacter($immune,$c)
+    public function encodeCharacter($immune, $c)
     {
-    	//detect encoding, special-handling for chr(172) and chr(128) to chr(159) which fail to be detected by mb_detect_encoding()
-  		$initialEncoding = $this->detectEncoding($c);
-
-    	// Normalize encoding to UTF-32
-  		$_4ByteUnencodedOutput = $this->normalizeEncoding($c);
-  		
-  		// Start with nothing; format it to match the encoding of the string passed as an argument.
-  		$encodedOutput = mb_convert_encoding("", $initialEncoding);
-    
-  		// Grab the 4 byte character.
-  		$_4ByteCharacter = $this->forceToSingleCharacter($_4ByteUnencodedOutput);
-  		
-  		// Get the ordinal value of the character.
-  		list(, $ordinalValue) = unpack("N", $_4ByteCharacter);
-  		
+        //detect encoding, special-handling for chr(172) and chr(128) to chr(159) 
+        //which fail to be detected by mb_detect_encoding()
+        $initialEncoding = $this->detectEncoding($c);
+        
+        // Normalize encoding to UTF-32
+        $_4ByteUnencodedOutput = $this->normalizeEncoding($c);
+        
+        // Start with nothing; format it to match the encoding of the string passed 
+        //as an argument.
+        $encodedOutput = mb_convert_encoding("", $initialEncoding);
+        
+        // Grab the 4 byte character.
+        $_4ByteCharacter = $this->forceToSingleCharacter($_4ByteUnencodedOutput);
+        
+        // Get the ordinal value of the character.
+        list(, $ordinalValue) = unpack("N", $_4ByteCharacter);
+        
         // check for immune characters
-  		foreach($immune as $immuneCharacter)
-  		{
-  			// Convert to UTF-32 (4 byte characters, regardless of actual number of bytes in the character).
-  			$_4ByteImmuneCharacter = $this->normalizeEncoding($immuneCharacter);
-  			
-  			// Ensure it's a single 4 byte character (since $immune is an array of strings) by grabbing only the 1st multi-byte character.
-  			$_4ByteImmuneCharacter = $this->forceToSingleCharacter($_4ByteImmuneCharacter);
-  			
-  			// If the character is immune then return it.
-  			if($_4ByteCharacter === $_4ByteImmuneCharacter)
-  			{
-  				return $encodedOutput.chr($ordinalValue);
-  			}
-  		}
-    	
-    	// Check for alphanumeric characters
-  		$hex = $this->getHexForNonAlphanumeric($_4ByteCharacter);
-  		if($hex===null)
-  		{
-  			return $encodedOutput.chr($ordinalValue);
-  		}
-		
-		  return $encodedOutput."^".$c;
+        foreach ($immune as $immuneCharacter) {
+            // Convert to UTF-32 (4 byte characters, regardless of actual number of 
+            //bytes in the character).
+            $_4ByteImmuneCharacter = $this->normalizeEncoding($immuneCharacter);
+            
+            // Ensure it's a single 4 byte character (since $immune is an array of 
+            //strings) by grabbing only the 1st multi-byte character.
+            $_4ByteImmuneCharacter = $this->forceToSingleCharacter(
+                $_4ByteImmuneCharacter
+            );
+            
+            // If the character is immune then return it.
+            if ($_4ByteCharacter === $_4ByteImmuneCharacter) {
+                return $encodedOutput . chr($ordinalValue);
+            }
+        }
+        
+        // Check for alphanumeric characters
+        $hex = $this->getHexForNonAlphanumeric($_4ByteCharacter);
+        if ($hex === null) {
+            return $encodedOutput . chr($ordinalValue);
+        }
+        
+        return $encodedOutput . "^" . $c;
     }
-
- 
+    
+    
     
     /**
      * {@inheritDoc}
      */
     public function decodeCharacter($input)
     {
-    	// Assumption/prerequisite: $c is a UTF-32 encoded string
-		  $_4ByteEncodedInput = $input;
-    
-    	if(mb_substr($_4ByteEncodedInput,0,1,"UTF-32") === null)
-    	{
-    		// 1st character is null, so return null
-    		// eat the 1st character off the string and return null
-    		$_4ByteEncodedInput = mb_substr($input,1,mb_strlen($_4ByteEncodedInput,"UTF-32"),"UTF-32");	//no point in doing this
-    		return array('decodedCharacter'=>null,'encodedString'=>null);
-    	}
-    	
-    	// if this is not an encoded character, return null
-    	if(mb_substr($_4ByteEncodedInput,0,1,"UTF-32") != $this->normalizeEncoding('^'))
-    	{
-    		// 1st character is not part of encoding pattern, so return null
-    		return array('decodedCharacter'=>null,'encodedString'=>null);
-    	}
-    	
-    	// 1st character is part of encoding pattern...
-    	$second = mb_substr($_4ByteEncodedInput,1,1,"UTF-32");
-    	
-    	return array('decodedCharacter'=>$second,'encodedString'=>mb_substr($input,0,2,"UTF-32"));
+        // Assumption/prerequisite: $c is a UTF-32 encoded string
+        $_4ByteEncodedInput = $input;
+        
+        if (mb_substr($_4ByteEncodedInput, 0, 1, "UTF-32") === null) {
+            // 1st character is null, so return null
+            // eat the 1st character off the string and return null
+            //todo: no point in doing this
+            $_4ByteEncodedInput = mb_substr(
+                $input, 1, mb_strlen($_4ByteEncodedInput, "UTF-32"), "UTF-32"
+            );
+            return array(
+                'decodedCharacter' => null,
+                'encodedString' => null
+            );
+        }
+        
+        // if this is not an encoded character, return null
+        if (mb_substr($_4ByteEncodedInput, 0, 1, "UTF-32") != $this->normalizeEncoding('^')) {
+            // 1st character is not part of encoding pattern, so return null
+            return array(
+                'decodedCharacter' => null,
+                'encodedString' => null
+            );
+        }
+        
+        // 1st character is part of encoding pattern...
+        $second = mb_substr($_4ByteEncodedInput, 1, 1, "UTF-32");
+        
+        return array(
+            'decodedCharacter' => $second,
+            'encodedString' => mb_substr($input, 0, 2, "UTF-32")
+        );
     }
 }
